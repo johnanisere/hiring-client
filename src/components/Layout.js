8387590cc589426fc0604017228c5fa135d149ee
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-08 10:27:28 +0100

    Adds Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import { Grommet, Box, Grid } from 'grommet';
+import { grommet } from 'grommet/themes';
+import Header from './Header';
+import SideBar from './SideBar';
+
+export default function Layout(props) {
+  const [sidebar, setSidebar] = useState(true);
+  console.log(sidebar);
+
+  function toggleSidebar() {
+    setSidebar(!sidebar);
+    console.log(sidebar);
+  }
+
+  return (
+    <Grommet full theme={grommet}>
+      <Grid
+        fill
+        rows={['auto', 'flex']}
+        columns={['auto', 'flex']}
+        areas={[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'sidebar', start: [0, 1], end: [0, 1] },
+          { name: 'main', start: [1, 1], end: [1, 1] }
+        ]}
+      >
+        <Header toggleSidebar={toggleSidebar} />
+        <SideBar sidebar={sidebar} />
+        <Box gridArea="main" justify="center" align="center">
+          {props.children}
+        </Box>
+      </Grid>
+    </Grommet>
+  );
+}
