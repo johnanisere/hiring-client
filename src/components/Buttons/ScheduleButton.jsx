b021f854ada9ce9d3e8a392b183b19d60fd7f16e
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-08 10:08:35 +0100

    add the props for handling actions

--- src/components/Buttons/ScheduleButton.jsx
+++ src/components/Buttons/ScheduleButton.jsx
@@ -4,11 +4,11 @@ import { Add } from "grommet-icons";
 import { Box, Button, Grommet } from "grommet";
 import { grommet } from "grommet/themes";
 
-const IconLabel = () => (
+const IconLabel = ({action}) => (
   <Grommet theme={grommet}>
     <Box align="center" pad="large">
       <Box round="full" overflow="hidden" background="neutral-1">
-        <Button icon={<Add />} hoverIndicator onClick={() => {}} />
+        <Button icon={<Add />} hoverIndicator onClick={action} />
       </Box>
     </Box>
   </Grommet>
