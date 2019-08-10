e42927adbf38cf38ce03ec023102844be488d47e
seun jay <seunjay92@gmail.com>
2019-08-10 21:09:22 +0100

    Define function for DecaDev details

---
+++ src/components/profile/getDetails.js
@@ -0,0 +1,10 @@
+export function getDetails() {
+  return {
+    name: "John Doe",
+    resume: "myResume",
+    email: "johndoe@gmail.com",
+    phone: "+2348099999999",
+    github: "github.com/jdoe",
+    squad: "Sq 003"
+  };
+}
