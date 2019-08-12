032cafb5dd2de33bb0e65ce0dfacae48e899597c
seun jay <seunjay92@gmail.com>
2019-08-12 15:27:35 +0100

    create InviteForm component

--- src/components/mailInvite/InviteForm.js
+++ src/components/mailInvite/InviteForm.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-import { Box, Form, Select, Button, Heading, Grommet} from "grommet";
-import { grommet } from "grommet/themes";
-
+import { Box,  Select, Button, Heading,  } from "grommet";
 
 export default function InviteForm() {
-  
   const [state, setState] = useState({
     options: ["Squad 1", "Squad 2", "Squad 3"],
     value: ""
@@ -13,28 +10,31 @@ export default function InviteForm() {
   const { options, value } = state;
   return (
     <>
-      <Grommet full theme={grommet}>
-        
+      {/* <Heading level={3} style={{ marginLeft: "0x", textAlign: "center" }}>Send Invite</Heading> */}
+      <div className="container">
+        <Heading
+          level={3}
+          color="dark-1"
+          style={{ marginLeft: "0x", textAlign: "center" }}
+        >
+          Send Invite
+        </Heading>
         <Box fill align="center" justify="start" pad="large">
-          <Form>
-            <Heading align="center">Invite Devs</Heading>
-            <Select
-              id="select"
-              name="select"
-              placeholder="Select Squad"
-              value={value}
-              options={options}
-              onChange={option => setState({ ...state, value: option })}
-            />
-          </Form>
-
+          <Select
+            id="select"
+            name="select"
+            placeholder="Select Squad"
+            value={value}
+            options={options}
+            onChange={option => setState({ ...state, value: option })}
+          />
           <Box align="start" pad="small">
             <Box direction="row" align="start" gap="small" pad="xsmall">
               <Button label="Send Invite" onClick={() => {}} />
             </Box>
           </Box>
         </Box>
-      </Grommet>
+      </div>
     </>
   );
 }
