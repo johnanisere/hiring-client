be48fe27b1967a68daf4aabd24eb6c8dcb4f9efd
seun jay <seunjay92@gmail.com>
2019-08-12 16:38:55 +0100

    Add heading

--- src/components/mailInvite/InviteForm.js
+++ src/components/mailInvite/InviteForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Box, Heading, Select, Button } from "grommet";
+import { Box, Form, Select, Button, Heading, Grommet} from "grommet";
+import { grommet } from "grommet/themes";
+
 
 export default function InviteForm() {
+  
   const [state, setState] = useState({
     options: ["Squad 1", "Squad 2", "Squad 3"],
     value: ""
@@ -10,31 +13,28 @@ export default function InviteForm() {
   const { options, value } = state;
   return (
     <>
-      {/* <Heading level={3} style={{ marginLeft: "0x", textAlign: "center" }}>Send Invite</Heading> */}
-      <div className="container">
-        <Heading
-          level={3}
-          color="dark-1"
-          style={{ marginLeft: "0x", textAlign: "center" }}
-        >
-          Send Invite
-        </Heading>
+      <Grommet full theme={grommet}>
+        
         <Box fill align="center" justify="start" pad="large">
-          <Select
-            id="select"
-            name="select"
-            placeholder="Select Squad"
-            value={value}
-            options={options}
-            onChange={option => setState({ ...state, value: option })}
-          />
+          <Form>
+            <Heading align="center">Send Invite</Heading>
+            <Select
+              id="select"
+              name="select"
+              placeholder="Select Squad"
+              value={value}
+              options={options}
+              onChange={option => setState({ ...state, value: option })}
+            />
+          </Form>
+
           <Box align="start" pad="small">
             <Box direction="row" align="start" gap="small" pad="xsmall">
               <Button label="Send Invite" onClick={() => {}} />
             </Box>
           </Box>
         </Box>
-      </div>
+      </Grommet>
     </>
   );
 }
