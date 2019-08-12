65572b28ac95d1f605cd73116865ca13077a7358
seun jay <seunjay92@gmail.com>
2019-08-12 15:28:50 +0100

    import ResponsiveContext

--- src/components/profile/Background.js
+++ src/components/profile/Background.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grommet, Box, Grid } from "grommet";
+import { Grommet, Box, Grid, ResponsiveContext } from "grommet";
 import { grommet } from "grommet/themes";
 import Photo from "./Photo";
 import Info from "./Info";
@@ -9,24 +9,28 @@ import Skills from "./Skills"
 const Background = () => {
   return (
     <Grommet theme={grommet} full>
-      <Grid
-        fill
-        areas={[
-          { name: "nav", start: [0, 0], end: [0, 0] },
-          { name: "main", start: [1, 0], end: [1, 0] }
-        ]}
-        columns={["medium", "flex"]}
-        rows={["flex"]}
-        gap="small"
-      >
-        <Box gridArea="nav" background="">
-          <Photo />
-          <Skills/>
-        </Box>
-        <Box gridArea="main" background="" style={{ marginLeft: "50px" }}>
-          <Info/>
-        </Box>
-      </Grid>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Grid
+            fill
+            areas={[
+              { name: "nav", start: [0, 0], end: [0, 0] },
+              { name: "main", start: [1, 0], end: [1, 0] }
+            ]}
+            columns={["medium", "flex"]}
+            rows={["flex"]}
+            gap="small"
+          >
+            <Box gridArea="nav" background="">
+              <Photo />
+              <Skills />
+            </Box>
+            <Box gridArea="main" background="" style={{ marginLeft: "50px" }}>
+              <Info />
+            </Box>
+          </Grid>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
   );
 };
