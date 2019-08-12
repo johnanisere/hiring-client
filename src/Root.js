3b739cf7a0c9684af963e5968ee7b6c395db2875
seun jay <seunjay92@gmail.com>
2019-08-12 17:29:25 +0100

    import Background

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import InviteForm from "../src/components/mailInvite/InviteForm";
+import Background from "../src/components/profile/Background"
 
 const Home = lazy(() => import("./compositions/Home"));
 
@@ -9,6 +10,7 @@ const App = () => (
     <Switch>
       <Suspense fallback="loading..">
         <Route exact path="/" component={Home} />
+        <Route path="/profile" component={Background}/>
         <Route path="/invitemail" component={InviteForm} />
       </Suspense>
     </Switch>
