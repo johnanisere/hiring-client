94a4acaf922f5ab10e8287fd87385699c219a6f9
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-13 18:13:54 +0100

    imports login reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,4 @@
 import { combineReducers } from 'redux';
+import login from './compositions/Authentication/reducer';
 
-export default combineReducers({});
+export default combineReducers({ login });
