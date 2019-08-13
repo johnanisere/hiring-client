a2463767b0a17a45500f1958df2442c6603a1b7c
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-13 18:13:54 +0100

    imports login reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import user from './components/Authentication/user.reducer';
-import authentication from './components/Authentication/authentication.reducer';
+import { combineReducers } from "redux";
+import user from "./components/Authentication/user.reducer";
+import authentication from "./components/Authentication/authentication.reducer";
 
 export default combineReducers({ authentication, user });
