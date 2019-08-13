c11d8b3784ce9becf6e60d7d03be2fb622b6d59a
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-13 10:05:09 +0100

    Adds authentication route in app

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,19 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import InviteForm from "../src/components/mailInvite/InviteForm";
-import Background from "../src/components/profile/Background"
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import InviteForm from '../src/components/mailInvite/InviteForm';
+import Background from '../src/components/profile/Background';
 
-const Home = lazy(() => import("./compositions/Home"));
+const Home = lazy(() => import('./compositions/Home'));
+const Authentication = lazy(() => import('./components/Authentication'));
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Suspense fallback="loading..">
         <Route exact path="/" component={Home} />
-        <Route path="/profile" component={Background}/>
+        <Route path="/profile" component={Background} />
         <Route path="/invitemail" component={InviteForm} />
+        <Route to="/" component={Authentication} />
       </Suspense>
     </Switch>
   </BrowserRouter>
