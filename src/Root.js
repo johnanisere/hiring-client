6af236ea1c793d08c573b07f2324e8bede241900
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-13 10:05:09 +0100

    Adds authentication route in app

--- src/Root.js
+++ src/Root.js
@@ -2,11 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Home = lazy(() => import('./compositions/Home'));
+const Authentication = lazy(() => import('./components/Authentication'));
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Suspense fallback="loading..">
+        <Route to="/" component={Authentication} />
         <Route to="/" component={Home} />
       </Suspense>
     </Switch>
