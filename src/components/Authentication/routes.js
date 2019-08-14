99f0e693f2ffac68f4df01a8d5bbecce0af91bad
seun jay <seunjay92@gmail.com>
2019-08-14 09:11:41 +0100

    uncomment UpdatePassword component

--- src/components/Authentication/routes.js
+++ src/components/Authentication/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
-// import UpdatePassword from './UpdatePassword';
+import UpdatePassword from './UpdatePassword';
 import Login from './Login';
 
 export default function({ match: { path } }) {
   return (
     <Switch>
-      {/* <Route to={`${path}/update-password`} component={UpdatePassword} /> */}
+      <Route to={`${path}/update-password`} component={UpdatePassword} />
       <Route to={`${path}/login`} component={Login} />
     </Switch>
   );
