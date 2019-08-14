bf23f1bbf91ce2cdb680f54ec630597e9dfab22e
seun jay <seunjay92@gmail.com>
2019-08-14 06:55:35 +0100

    import axios

--- src/components/mailInvite/InviteForm.js
+++ src/components/mailInvite/InviteForm.js
@@ -1,35 +1,49 @@
 import React, { useState } from "react";
 import { Box, Form, Select, Button, Heading, Grommet } from "grommet";
 import { grommet } from "grommet/themes";
+import axios from "axios";
 
 export default function InviteForm() {
-  const [state, setState] = useState({
+  const [squad, setSquad] = useState({
     options: ["Squad 1", "Squad 2", "Squad 3"],
     value: ""
   });
 
-  const { options, value } = state;
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("submitted");
+    console.log(value)
+    //axios.post("http://localhost:3005/api/v1//invite/devs");
+  };
+
+    const { options, value } = squad;
+
   return (
     <>
       <Grommet full theme={grommet}>
         <Box fill align="center" justify="start" pad="large">
-          <Form>
-            <Heading align="center">Invite Devs</Heading>
-            <Select
-              id="select"
-              name="select"
-              placeholder="Select Squad"
-              value={value}
-              options={options}
-              onChange={option => setState({ ...state, value: option })}
-            />
-          </Form>
-
-          <Box align="start" pad="small">
-            <Box direction="row" align="start" gap="small" pad="xsmall">
-              <Button label="Send Invite" onClick={() => {}} />
-            </Box>
-          </Box>
+          <div className="container">
+            <Form onSubmit={handleSubmit}>
+              <Heading align="center" style={{textAlign: "center"}}>Invite Devs</Heading>
+              <Select
+                id="select"
+                name="select"
+                placeholder="Select Squad"
+                value={value}
+                options={options}
+                onChange={ ({option}) => setSquad({...squad, value: option })}
+              />
+              <Box align="start" pad="small">
+                <Box direction="row" align="start" gap="small" pad="xsmall">
+                  <Button
+                    label="Send Invite"
+                    type="submit"
+                    style={{ marginLeft: "45px" }}
+                  />
+                </Box>
+              </Box>
+            </Form>
+          </div>
         </Box>
       </Grommet>
     </>
