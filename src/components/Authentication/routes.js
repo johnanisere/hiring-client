8bc07b510cb1416e37a849adbfbd31887fbeada8
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-14 13:25:20 +0100

    add the component to route

--- src/components/Authentication/routes.js
+++ src/components/Authentication/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 // import UpdatePassword from './UpdatePassword';
 import Login from './Login';
+import ChangePassword from '../passwordPage/changePass'
 
 export default function({ match: { path } }) {
   return (
     <Switch>
       {/* <Route to={`${path}/update-password`} component={UpdatePassword} /> */}
       <Route to={`${path}/login`} component={Login} />
+      <Route to={`${path}/change-password`} component={ChangePassword} />
     </Switch>
   );
 }
