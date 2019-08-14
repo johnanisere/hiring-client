5e670ccf0d8fb55542c201b2ae040f65a2e55031
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-14 15:45:38 +0100

    adds login action

---
+++ src/compositions/Authentication/login.action.js
@@ -0,0 +1,38 @@
+export const USER_LOGIN = 'USER_LOGIN';
+const login = payload => ({
+  type: USER_LOGIN,
+  payload
+});
+
+const setToken = payload => ({
+  type: 'SET_TOKEN',
+  payload
+});
+
+const setLoading = payload => ({
+  type: 'LOADING',
+  payload
+});
+const onError = payload => ({
+  type: 'USER_LOGIN_ERROR',
+  payload
+});
+
+const loginBoundActionCreator = (data, request) => async dispatch => {
+  try {
+    dispatch(setLoading(true));
+    const response = await request({
+      method: 'POST',
+      url: '/users/login',
+      data
+    });
+    dispatch(login(response.data));
+    dispatch(setToken(response.data.token));
+    dispatch(setLoading(false));
+  } catch (err) {
+    dispatch(setLoading(false));
+    dispatch(onError(err));
+  }
+};
+
+export default loginBoundActionCreator;
