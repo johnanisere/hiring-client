92af06f4809d9a344f9f36c8ee46d5b3cc763c8d
seun jay <seunjay92@gmail.com>
2019-08-14 14:11:58 +0100

    comment code

---
+++ src/components/Authentication/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Route, Switch } from "react-router-dom";
+import UpdatePassword from "./UpdatePassword";
+//import Login from "./Login";
+
+export default function({ match: { path } }) {
+  return (
+    <Switch>
+      <Route to={`${path}/update-password`} component={UpdatePassword} />
+      {/* <Route to={`${path}/login`} component={Login} /> */}
+    </Switch>
+  );
+}
