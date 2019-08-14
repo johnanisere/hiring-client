30c90ce6e9a8b7222b4f8266b481fa5a4676fe92
seun jay <seunjay92@gmail.com>
2019-08-14 09:23:17 +0100

    import Cards component

--- src/compositions/Home.js
+++ src/compositions/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import Layout from "../components/Layout";
+import Cards from "../components/Cards";
 
 function App(props) {
   return (
     <div className="App">
-      <Layout />
+      <Layout>
+        <Cards />
+      </Layout>
     </div>
   );
 }
