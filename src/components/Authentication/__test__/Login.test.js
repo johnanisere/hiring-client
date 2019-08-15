db52b3970fec234967f1b600e296e7f2854733cb
john anisere <johnanisere@gmail.com>
2019-08-15 19:13:15 +0100

    implement fix

--- src/components/Authentication/__test__/Login.test.js
+++ src/components/Authentication/__test__/Login.test.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, configure } from "enzyme";
 import Login from "../Login";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
 
+// React 16 Enzyme adapter
+configure({ adapter: new Adapter() });
 const mockStore = configureStore();
 const store = mockStore();
 
