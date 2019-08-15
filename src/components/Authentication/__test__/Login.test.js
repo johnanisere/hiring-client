20f16a9ce89973a0e66cb3a78605be2486b42d95
john anisere <johnanisere@gmail.com>
2019-08-15 19:04:37 +0100

    implement fix

--- src/components/Authentication/__test__/Login.test.js
+++ src/components/Authentication/__test__/Login.test.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import { shallow } from "enzyme";
+import { shallow } from "enzyme";
 import Login from "../Login";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-
 import toJson from "enzyme-to-json";
 
 const mockStore = configureStore();
