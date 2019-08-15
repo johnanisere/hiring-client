823bae6b0890e80b0e49b86d392379037f3de01a
john anisere <johnanisere@gmail.com>
2019-08-15 17:28:04 +0100

    fix

--- src/components/Authentication/__test__/login.action.test.js
+++ src/components/Authentication/__test__/login.action.test.js
@@ -1,4 +1,5 @@
 import loginBoundActionCreator from '../login.action';
+
 function setUp() {
   const dispatch = async val => val;
   const data = {
--- src/components/input/__test__/input.test.js
+++ src/components/input/__test__/input.test.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import Input from '../index';
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Input from "../index";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-describe('<Input />', () => {
-  describe('render()', () => {
-    test('renders the component', () => {
+configure({ adapter: new Adapter() });
+describe("<Input />", () => {
+  describe("render()", () => {
+    test("renders the component", () => {
       const onChange = jest.fn();
 
       const wrapper = shallow(
@@ -13,7 +16,7 @@ describe('<Input />', () => {
           validate={{
             regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
             message:
-              'Password must contain at least 8 characters, 1 letter, and 1 number'
+              "Password must contain at least 8 characters, 1 letter, and 1 number"
           }}
           placeholder="dummyPlaceholder"
           value="dummyValue"
