f38883d3658544367ee856a29b4667bb06e5d7ce
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-15 11:22:23 +0100

    modifies login.action test

--- src/components/Authentication/__test__/login.action.test.js
+++ src/components/Authentication/__test__/login.action.test.js
@@ -1,34 +1,34 @@
-import loginBoundActionCreator from "../login.action";
+import loginBoundActionCreator from '../login.action';
 function setUp() {
   const dispatch = async val => val;
   const data = {
-    email: "j@gmail.com",
-    password: "weybudjcbdc"
+    email: 'j@gmail.com',
+    password: 'weybudjcbdc'
   };
   return { data, dispatch };
 }
-describe("fetchCoinAction", () => {
-  it("should call api and update coins state", done => {
+describe('loginBoundActionCreator', () => {
+  it('should call api and update coins state', done => {
     const request = {
       post: () =>
         Promise.resolve({
-          data: "dummy data"
+          data: 'dummy data'
         })
     };
     const { data, dispatch } = setUp();
 
-    const getSpy = jest.spyOn(request, "post");
+    const getSpy = jest.spyOn(request, 'post');
 
     loginBoundActionCreator(data, request)(dispatch)
       .then(response => {
-        expect(response).toBe("dummy data");
+        expect(response).toBe('dummy data');
         done();
       })
       .catch(() => {
-        fail("should not catch");
+        fail('should not catch');
       });
 
-    expect(getSpy).toBeCalledWith("/users/login", data);
+    expect(getSpy).toBeCalledWith('/users/login', data);
     expect(getSpy).toBeCalledTimes(1);
   });
 });
