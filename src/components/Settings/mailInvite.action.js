a0ff75c4d6232448bd9e01be41861ea862c2139f
seun jay <seunjay92@gmail.com>
2019-08-15 11:45:16 +0100

    define mailInvite actions

---
+++ src/components/Settings/mailInvite.action.js
@@ -0,0 +1,30 @@
+export const DEVS_MAILINVITE = "DEVS_MAILINVITE";
+
+const mailInvite = payload => ({
+  type: DEVS_MAILINVITE,
+  payload
+});
+
+const setLoading = payload => ({
+  type: "LOADING",
+  payload
+});
+
+const onError = payload => ({
+  type: "MAIL_INVITE_ERROR",
+  payload
+});
+
+const mailInviteBoundActionCreator = (data, request) => async dispatch => {
+  try {
+    dispatch(setLoading(true));
+    const response = await request.post("/invite/devs", data);
+    dispatch(mailInvite(response.data));
+    dispatch(setLoading(false));
+  } catch (error) {
+    dispatch(setLoading(false));
+    return dispatch(onError(error));
+  }
+};
+
+export default mailInviteBoundActionCreator;
