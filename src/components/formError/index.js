27dad29e099a172da5336616e5f99ae59cddde68
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-15 17:11:48 +0100

    abstracts form error

---
+++ src/components/formError/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import { errorMessageExtrator } from '../../helpers/utils';
+
+const formError = ({ error }) =>
+  error && (
+    <small
+      className="error"
+      style={{
+        color: 'red',
+        textAlign: 'center'
+      }}
+    >
+      {errorMessageExtrator(error)}
+    </small>
+  );
+
+formError.propTypes = {
+  error: Proptypes.object
+};
+export default formError;
