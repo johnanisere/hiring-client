064be3eeaa23126a7d02b6e718badd49f0d236ac
seun jay <seunjay92@gmail.com>
2019-08-15 11:42:01 +0100

    change file path to InviteForm component

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import InviteForm from "./components/Settings/InviteForm";
-import Background from "../src/components/profile/Background";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import InviteForm from '../src/components/Settings/InviteForm';
+import Background from '../src/components/profile/Background';
 
 const Home = lazy(() => import("./compositions/Home"));
 const Authentication = lazy(() => import("./compositions/authentication"));
