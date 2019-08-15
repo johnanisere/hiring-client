a0835b7b56f79ee958d21b32353d7289212237ec
john anisere <johnanisere@gmail.com>
2019-08-15 17:35:58 +0100

    fix

--- src/components/Authentication/__test__/Login.test.js
+++ src/components/Authentication/__test__/Login.test.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Login from '../Login';
-import toJson from 'enzyme-to-json';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import React from "react";
+import { shallow } from "enzyme";
+import Login from "../Login";
+import toJson from "enzyme-to-json";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
+configure({ adapter: new Adapter() });
 const mockStore = configureStore();
 const store = mockStore();
 
-describe('<Login />', () => {
-  describe('render()', () => {
-    test('renders the component', () => {
+describe("<Login />", () => {
+  describe("render()", () => {
+    test("renders the component", () => {
       const wrapper = shallow(
         <Provider store={store}>
           <Login />
