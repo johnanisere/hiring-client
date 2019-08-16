5ff5308587c6aa686026da39157a9ac4bbbe187a
seun jay <seunjay92@gmail.com>
2019-08-16 15:50:57 +0100

    remove mailInvite reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import user from "./components/Authentication/user.reducer";
 import authentication from "./components/Authentication/authentication.reducer";
-import mailInvite from "./components/Settings/mail.reducer";
 
-export default combineReducers({ authentication, user, mailInvite });
+export default combineReducers({ authentication, user });
