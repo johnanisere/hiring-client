e5208eddd1f014fbcba2f7f83a7c130a410447ea
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-16 01:32:41 +0100

    init

--- src/components/passwordPage/changePass.js
+++ src/components/passwordPage/changePass.js
@@ -1,36 +1,34 @@
-import React,{useState} from 'react';
-import { Box, Button, Form, Text, FormField } from 'grommet';
-import {connect} from 'react-redux';
-import { BeatLoader } from 'react-spinners';
-import request from '../../request';
-import changePassBoundActionCreator from './changePass.action';
+import React, { useState } from "react";
+import { Box, Button, Form, Text, FormField } from "grommet";
+import { connect } from "react-redux";
+import { BeatLoader } from "react-spinners";
+import request from "../../request";
+import changePassBoundActionCreator from "./changePass.action";
 
-const ChangePass = ({ changePass }) =>  {
-  const {error,onError}=useState("");
-  const {loading,onLoading}=useState(false)
-  const {success,onSuccess}=useState("")
-  const {data,onChnageData}=useState({
-    email:"",
-    newPassword:"",
-    confirmPasword:""
-  })
+const ChangePass = ({ changePass }) => {
+  const { error, onError } = useState("");
+  const { loading, onLoading } = useState(false);
+  const { success, onSuccess } = useState("");
+  const { data, onChnageData } = useState({
+    email: "",
+    newPassword: "",
+    confirmPasword: ""
+  });
 
   // collects the data from the fields
-  const handleChange = ({target:{name,value}})=> {
-    onChnageData({...data,[name]:value})
+  const handleChange = ({ target: { name, value } }) => {
+    onChnageData({ ...data, [name]: value });
   };
-const activityIndicator=()=>{
-  onLoading(!loading)
-}
-const handleError=(error)=>{
-  onError(error)
-}
+  const activityIndicator = () => {
+    onLoading(!loading);
+  };
+  const handleError = error => {
+    onError(error);
+  };
   //submit the data to the backend
-  const handleSubmit = async(e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-
-    changePass(data, request, activityIndicator,handleError,onSuccess);
-
+    changePass(data, request, activityIndicator, handleError, onSuccess);
   };
 
   // renders the layout
@@ -42,9 +40,9 @@ const handleError=(error)=>{
           size="large"
           margin="auto"
           style={{
-            fontWeight: 'bold',
-            fontSize: '25px',
-            paddingBottom: '15px'
+            fontWeight: "bold",
+            fontSize: "25px",
+            paddingBottom: "15px"
           }}
         >
           Change Password
@@ -54,7 +52,7 @@ const handleError=(error)=>{
         </Text>
       </Box>
       {error && (
-        <small className="error" style={{ color: 'red', textAlign: 'center' }}>
+        <small className="error" style={{ color: "red", textAlign: "center" }}>
           Make sure email and password are valid!
         </small>
       )}
@@ -64,15 +62,15 @@ const handleError=(error)=>{
           required
           validate={{
             regexp: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-            message: 'Input must be valid email!'
+            message: "Input must be valid email!"
           }}
           onChange={handleChange}
           placeholder="Email"
           type="email"
           value={data.email}
           style={{
-            marginBottom: '15px',
-            borderRadius: '20px'
+            marginBottom: "15px",
+            borderRadius: "20px"
           }}
         />
         <FormField
@@ -85,12 +83,12 @@ const handleError=(error)=>{
           validate={{
             regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
             message:
-              'Password must contain at least 8 characters, 1 letter, and 1 number'
+              "Password must contain at least 8 characters, 1 letter, and 1 number"
           }}
           color="dark-1"
           style={{
-            marginBottom: '15px',
-            borderRadius: '20px'
+            marginBottom: "15px",
+            borderRadius: "20px"
           }}
         />
         <FormField
@@ -102,38 +100,40 @@ const handleError=(error)=>{
           validate={{
             regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
             message:
-              'Password must contain at least 8 characters, 1 letter, and 1 number'
+              "Password must contain at least 8 characters, 1 letter, and 1 number"
           }}
           color="dark-1"
           value={data.confirmPassword}
           style={{
-            marginBottom: '15px',
-            borderRadius: '20px'
+            marginBottom: "15px",
+            borderRadius: "20px"
           }}
         />
         <Box
           direction="row"
           justify="center"
           align="center"
-          margin={{ top: 'medium' }}
+          margin={{ top: "medium" }}
         >
           <Button
             primary
             width="large"
             color="dark-1"
-            label={loading ? <BeatLoader size={5} color="#fff" /> : 'Submit'}
+            label={loading ? <BeatLoader size={5} color="#fff" /> : "Submit"}
             type="submit"
-            style={{ width: '100%', marginTop: 20 }}
+            style={{ width: "100%", marginTop: 20 }}
           />
         </Box>
       </Form>
     </>
   );
-}
+};
 
 const mapDispatchToProps = {
   changePass: changePassBoundActionCreator
-}
+};
 
-export default connect(null, mapDispatchToProps)(ChangePass);
-                                                      
+export default connect(
+  null,
+  mapDispatchToProps
+)(ChangePass);
