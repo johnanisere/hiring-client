16ad782f506b13a75907065b256f9007d6407833
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-19 11:17:25 +0100

    modifies files due to move of decadevs actions and reducers

---
+++ src/components/Cards.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import request from '../request';
+import { getAllDecadevs } from './dashboard/decadevs-actions/decadevs.action';
+import {
+  Grommet,
+  Anchor,
+  Box,
+  Text,
+  Image,
+  Grid,
+  ResponsiveContext
+} from 'grommet';
+
+function Cards({ getAllDecadevs, decadevs }) {
+  useEffect(() => {
+    if (decadevs.length === 0) {
+      getAllDecadevs(request);
+    }
+  }, [decadevs, getAllDecadevs]);
+  return (
+    <Grommet style={{ overflow: 'scroll', minHeight: '100%' }}>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Grid
+            columns={
+              size === 'small'
+                ? ['1']
+                : size === 'medium'
+                ? ['1/2', '1/2']
+                : size === 'large'
+                ? ['1/4', '1/4', '1/4', '1/4']
+                : size === 'xlarge'
+                ? ['1/5', '1/5', '1/5', '1/5', '1/5']
+                : ['1/6', '1/6', '1/6', '1/6', '1/6', '1/6']
+            }
+          >
+            {decadevs.map(dev => {
+              return (
+                <Box
+                  key={dev._id}
+                  pad="medium"
+                  align="center"
+                  background={{
+                    color: 'light-2',
+                    opacity: 'strong'
+                  }}
+                  round
+                  gap="small"
+                  margin="medium"
+                >
+                  <Image
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      borderRadius: '12px'
+                    }}
+                    fit="cover"
+                    src={dev.profilePhoto}
+                  />
+                  <Text>{dev.email}</Text>
+                  <Anchor href="" label="More Info" />
+                </Box>
+              );
+            })}
+          </Grid>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    decadevs: state.decadevs.decadevs
+  };
+};
+const mapDispatchToProps = {
+  getAllDecadevs
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(React.memo(Cards));
