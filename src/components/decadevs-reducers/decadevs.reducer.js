69f87bda18e4d40ee458a93b56ed3bd6910dd6f9
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-19 10:54:29 +0100

    refactors decadevs reducers to include cases for loading and error activities

--- src/components/decadevs-reducers/decadevs.reducer.js
+++ src/components/decadevs-reducers/decadevs.reducer.js
@@ -1,14 +1,26 @@
 import { GET_ALL_DECADEVS } from '../decadevs-actions/decadevs.action';
 export const initialState = {
+  loading: false,
+  error: {},
   decadevs: []
 };
 
 export default function decadevs(state = initialState, action) {
   switch (action.type) {
+    case 'GET_ALL_DECADEVS_LOADING':
+      return {
+        ...state,
+        loading: action.payload
+      };
+    case 'GET_ALL_DECADEVS_ERROR':
+      return {
+        ...state,
+        error: action.payload
+      };
     case GET_ALL_DECADEVS:
       return {
         ...state,
-        decadevs: action.data
+        decadevs: action.payload
       };
     default:
       return state;
