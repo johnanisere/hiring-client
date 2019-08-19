b6a78a657e46b6549959fe39017b79fdf99913a5
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-19 14:31:48 +0100

    fixes

--- src/components/authentication/changePassword.action.js
+++ src/components/authentication/changePassword.action.js
@@ -7,7 +7,7 @@ const changePassBoundActionCreator = (
 ) => async () => {
   try {
     activityIndicator(true);
-    const response = await request.put("/change-password", data, {
+    const response = await request.put("/users/change-password", data, {
       headers: {
         authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphbmVkb2VAZXhhbXBsZS5jb20iLCJpYXQiOjE1NjU5NjAyOTYsImV4cCI6MTU2NTk2Mzg5Nn0.mwKMRibSeffTEamTYiuWlYy09dtBG57zAgY7ySWb-OE"
