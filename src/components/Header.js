bc1de2e9cfddf0f4ef56af354b8524718d549b6c
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-19 10:08:48 +0100

    resolve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Box, Button, Text } from 'grommet';
+import React from "react";
+import { Box, Button, Text } from "grommet";
+import { NavLink } from "react-router-dom";
 
 export default function Header(props) {
   return (
@@ -8,14 +9,15 @@ export default function Header(props) {
       direction="row"
       align="center"
       justify="between"
-      pad={{ horizontal: 'medium', vertical: 'small' }}
+      pad={{ horizontal: "medium", vertical: "small" }}
       background="dark-2"
     >
       <Button onClick={props.toggleSidebar}>
         <Text size="large">Title</Text>
       </Button>
-
-      <Text>email@example.com</Text>
+      <NavLink className="link" to="/updatepassword">
+        <Text>Update Password</Text>
+      </NavLink>
     </Box>
   );
 }
