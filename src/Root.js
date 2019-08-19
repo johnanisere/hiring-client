70b3988a2241e0dc9c8dfe845de9a2735104c7d8
seun jay <seunjay92@gmail.com>
2019-08-19 16:32:19 +0100

    fix

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import InviteForm from "./components/settings/InviteForm";
+import InviteForm from "./components/Settings/InviteForm";
 import Background from "../src/components/profile/Background";
 
 const Home = lazy(() => import("./compositions/Home"));
