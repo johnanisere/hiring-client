eaae7f4cf720da55fce85b9ce2f0c9bbb4cc5b60
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-19 10:52:23 +0100

    adds scheduleInterview functional component

--- src/components/ScheduleInterview.js
+++ src/components/ScheduleInterview.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import request from '../../request';
+
+// import { DateTime } from 'grommet/components/Form/';
+import { Box, Button, Grommet, Form, FormField, TextArea } from 'grommet';
+import { grommet } from 'grommet/themes';
 
 export default function ScheduleInterview() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Grommet full theme={grommet}>
+      <Box fill align="center" justify="center">
+        <Box width="medium">
+          <Form
+            onReset={event => console.log(event)}
+            onSubmit={({ value }) => console.log('Submit', value)}
+          >
+            <FormField
+              label="Add Title"
+              name="name"
+              required
+              validate={{ regexp: /^[a-z]/i }}
+            />
+            <FormField
+              label="Add Location"
+              name="email"
+              type="email"
+              required
+            />
+            <FormField
+              label="Add Guests"
+              name="employeeId"
+              required
+              validate={{ regexp: /^[0-9]{4,6}$/, message: '4-6 digits' }}
+            />
+            <FormField
+              label="Add Description"
+              name="comments"
+              component={TextArea}
+            />
+            {/* <FormField>
+              <Date id="id" name="name" format="M/D/YYYY" onChange="" />
+            </FormField> */}
+            <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+              <Button type="submit" label="Save Interview" primary />
+            </Box>
+          </Form>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
