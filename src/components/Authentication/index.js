00a5739a70a8ef3e8903a31710d702c5ef7f0d07
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-19 16:58:53 +0100

    fix

--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 const Login = lazy(() => import('./Login'));
 const SignUp = lazy(() => import('../SignUp/SignUp'));
 const UpdatePassword = lazy(() => import('./UpdatePassword'));
+const ChangePassword = lazy(() => import('./ChangePassword'));
 
 export default function({ match: { path }, login, loading, error }) {
   return (
@@ -12,6 +13,7 @@ export default function({ match: { path }, login, loading, error }) {
       <Route exact path={`/login`} component={Login} />
       <Route exact path={`/signup`} component={SignUp} />
       <Route exact path={`/update-password`} component={UpdatePassword} />
+      <Route exact path={`/change-password`} component={ChangePassword} />
     </Switch>
   );
 }
