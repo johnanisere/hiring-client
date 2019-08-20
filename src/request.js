2f5147c8ecdefed35dd187f07d386a8c2a788e34
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-20 14:19:30 +0100

    Validate password and confirmPassword. Display custom error messages.

--- src/request.js
+++ src/request.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-import store from "./store";
+import axios from 'axios';
+import store from './store';
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}/api/v1`
+  // baseURL: `${process.env.REACT_APP_BASE_URL}/api/v1`
+  baseURL: 'http://localhost:3005/api/v1'
 });
 
 instance.interceptors.request.use(config => {
