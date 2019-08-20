0f6afa2c95593f1ba206f650ea63edb462a9d057
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-20 14:19:30 +0100

    Validate password and confirmPassword. Display custom error messages.

--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -1,44 +1,62 @@
-import React, { useState } from "react";
-import request from "../../request";
-import { Box, Form, Text, FormField } from "grommet";
-import { useSelector, connect } from "react-redux";
-import signupBoundActionCreator from "./signup.action";
-import FormError from "../formError";
-import Button from "../button/FormButton";
+import React, { useState } from 'react';
+import request from '../../request';
+import { Box, Form, Text, FormField } from 'grommet';
+import { useSelector, connect } from 'react-redux';
+import signupBoundActionCreator from './signup.action';
+import FormError from '../formError';
+import Button from '../button/FormButton';
 function SignUp(props) {
-  const { error, loading } = useSelector(({ user }) => user);
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
-  });
+  let { error, loading } = useSelector(({ user }) => user);
 
-  
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
 
   const { name, email, password, confirmPassword } = values;
 
   const handleChange = e => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+    if (name === 'password') {
+      onvalidatePassword(value, values.confirmPassword);
+    } else if (name === 'confirmPassword') {
+      onvalidatePassword(values.password, value);
+    } else {
+      onvalidatePassword(values.password, values.confirmPassword);
+    }
+  };
+
+  const onvalidatePassword = (password, confirmPassword) => {
+    if (password !== confirmPassword) {
+      error.message = 'Passwords does not match';
+    } else {
+      error.message = undefined;
+    }
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.signup(values, request);
+    if (values.password !== values.confirmPassword) {
+      onvalidatePassword();
+    } else {
+      props.signup(values, request);
+    }
   };
 
   return (
     <>
-      <Box margin="small" pad="small" responsive>
+      <Box margin="small" pad="small" size="large" responsive>
         <Text
           width="auto"
           size="large"
           margin="auto"
           style={{
-            fontWeight: "bold",
-            fontSize: "25px",
-            paddingBottom: "15px"
+            fontWeight: 'bold',
+            fontSize: '25px',
+            paddingBottom: '15px'
           }}
         >
           SignUp
@@ -48,7 +66,7 @@ function SignUp(props) {
         </Text>
       </Box>
 
-      <FormError error={error} />
+      {error && <FormError error={error} />}
       <Form onSubmit={handleSubmit}>
         <FormField
           name="name"
@@ -57,8 +75,8 @@ function SignUp(props) {
           placeholder="Name"
           type="name"
           style={{
-            marginBottom: "5px",
-            borderRadius: "20px"
+            marginBottom: '5px',
+            borderRadius: '20px'
           }}
           required
         />
@@ -67,15 +85,15 @@ function SignUp(props) {
           required
           validate={{
             regexp: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-            message: "Input must be valid email!"
+            message: 'Input must be valid email!'
           }}
           value={email}
           onChange={handleChange}
           placeholder="Email"
           type="email"
           style={{
-            marginBottom: "5px",
-            borderRadius: "20px"
+            marginBottom: '5px',
+            borderRadius: '20px'
           }}
         />
         <FormField
@@ -86,8 +104,8 @@ function SignUp(props) {
           value={password}
           onChange={handleChange}
           style={{
-            marginBottom: "5px",
-            borderRadius: "20px"
+            marginBottom: '5px',
+            borderRadius: '20px'
           }}
         />
         <FormField
@@ -98,25 +116,20 @@ function SignUp(props) {
           placeholder="Confirm Password"
           type="password"
           style={{
-            marginBottom: "5px",
-            borderRadius: "20px"
+            marginBottom: '5px',
+            borderRadius: '20px'
           }}
         />
-        <Box
-          direction="row"
-          justify="center"
-          align="center"
-          margin={{ top: "medium" }}
-        >
-        </Box>
 
         <Box>
           <Button
             loading={loading}
             text="Sign Up"
-            type="submit"
+            primary
             width="large"
-            style={{ width: "100%" }}
+            label="Sign Up"
+            type="submit"
+            style={{ width: '100%' }}
           />
         </Box>
       </Form>
--- src/compositions/authentication/index.js
+++ src/compositions/authentication/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Authentication from '../../components/authentication/';
-import Formlayout from '../../components/FormLayout';
+import React, { Component } from "react";
+import Authentication from "../../components/authentication/";
+import Formlayout from "../../components/FormLayout";
 class App extends Component {
   render() {
     return (
