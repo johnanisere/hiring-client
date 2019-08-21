4eac40b7d29719e347dc135abff57ee9ea18b1bc
seun jay <seunjay92@gmail.com>
2019-08-21 14:24:58 +0100

    lazyload SignUp component

--- src/components/authentication/index.js
+++ src/components/authentication/index.js
@@ -3,6 +3,7 @@ import React, { lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 
 const Login = lazy(() => import("./Login"));
+const SignUp = lazy(() => import("./SignUp"));
 const UpdatePassword = lazy(() => import("./UpdatePassword"));
 const ChangePassword = lazy(() => import("./ChangePassword"));
 
@@ -10,6 +11,7 @@ export default function() {
   return (
     <Switch>
       <Route exact path={`/login`} component={Login} />
+      <Route exact path={`/signup`} component={SignUp} />
       <Route exact path={`/update-password`} component={UpdatePassword} />
       <Route exact path={`/change-password`} component={ChangePassword} />
     </Switch>
