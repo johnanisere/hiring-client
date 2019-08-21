946ee0ccf7949cd6ce509640bbd4fefe4ce7169b
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-21 17:41:48 +0100

    Adds form error test

--- src/components/formError/__tests__/index.test.js
+++ src/components/formError/__tests__/index.test.js
@@ -1,1 +1,37 @@
-// import { errorMessageExtrator } from '../../../compositions/Authentica';
+// import { errorMessageExtrator } from '../../../helpers/utils';
+import React from 'react';
+import { configure, mount, shallow } from 'enzyme';
+import FormError from '../index';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+const mockStore = configureStore();
+const store = mockStore();
+
+describe('For no Error', () => {
+  it('should not render the component', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <FormError />
+      </Provider>
+    );
+    expect(wrapper.find('small').exists()).toBeFalsy();
+  });
+});
+
+describe('For Error', () => {
+  it('should render where there is an error', () => {
+    // const errorMessageExtrator = jest.fn();
+    const wrapper = mount(
+      <Provider store={store}>
+        <FormError error="error" />
+      </Provider>
+    );
+    expect(wrapper.find('small').exists()).toBeTruthy();
+    // expect(errorMessageExtrator).toHaveBeenCalled();
+    // expect(wrapper.props('error')).toEqual('error');
+    // expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
