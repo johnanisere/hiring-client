be6608d8be4ce77bbbec669c308e702e2510b7d6
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-21 11:23:43 +0100

    adds dependencies for schedule interview component

--- src/compositions/Home.js
+++ src/compositions/Home.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-// import Layout from '../components/Layout';
-// import Cards from '../components/Cards';
-import ScheduleInterview from '../components/ScheduleInterview';
-function App(props) {
+import Layout from '../components/Layout';
+import Cards from '../components/dashboard/Cards';
+// import ScheduleInterview from '../components/dashboard/ScheduleInterview';
+export default function App(props) {
   return (
     <div className="App">
-      <ScheduleInterview />
-      {/* <Layout>
+      {/* <ScheduleInterview /> */}
+      <Layout>
         <Cards />
-      </Layout> */}
+      </Layout>
     </div>
   );
 }
-const mapStateToProps = state => ({ state });
-export default connect(mapStateToProps)(App);
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -11,3 +11,19 @@ export const errorMessageExtrator = error => {
     return error.message;
   }
 };
+
+export function toIso(date, time) {
+  let newDate = new Date(date);
+
+  let year = newDate.getFullYear(),
+    month = newDate.getMonth(),
+    day = newDate.getDate();
+
+  let x = time.split(':');
+
+  let hour = x[0].trim();
+  let min = x[1].trim();
+
+  let expectedDate = new Date(year, month, day, hour, min);
+  return expectedDate;
+}
