2899e70214b21f4c8bbc8466917dc890084346ba
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-22 00:00:48 +0100

    [Profile]: Designed Side View.

--- src/components/profile/Background.js
+++ src/components/profile/Background.js
@@ -1,33 +1,77 @@
-import React from "react";
-import { Grommet, Box, Grid, ResponsiveContext } from "grommet";
-import { grommet } from "grommet/themes";
-import Photo from "./Photo";
-import Info from "./Info";
-import Skills from "./Skills";
+import React from 'react';
+import { Grommet, Box, Grid, ResponsiveContext, Heading, Text } from 'grommet';
+import { grommet } from 'grommet/themes';
+import { Cloudlinux, User } from 'grommet-icons';
+
+import Photo from './Photo';
+import Info from './Info';
+import Skills from './Skills';
 
 const Background = () => {
   return (
     <Grommet theme={grommet} full>
       <ResponsiveContext.Consumer>
         {size => (
-          <Grid
+          <Box
+            responsive
             fill
-            areas={[
-              { name: "nav", start: [0, 0], end: [0, 0] },
-              { name: "main", start: [1, 0], end: [1, 0] }
-            ]}
-            columns={["medium", "flex"]}
-            rows={["flex"]}
-            gap="small"
+            background="#ffffff"
+            elevation="large"
+            round="small"
           >
-            <Box gridArea="nav" background="">
-              <Photo />
-              <Skills />
+            <Box
+              responsive
+              direction="row-responsive"
+              align="center"
+              pad={{ left: 'xlarge', right: 'xlarge' }}
+              margin={{ bottom: 'small' }}
+              style={{ boxShadow: '0px 1px 10px -8px' }}
+            >
+              <Box direction="row-responsive" gap="large" align="center">
+                <Cloudlinux color="plain" size="large" />
+                <Heading size="small">Decagon</Heading>
+              </Box>
+              <Box
+                style={{ marginLeft: 'auto' }}
+                direction="row-responsive"
+                gap="large"
+              >
+                <Text size="large"> Find People </Text>
+                <Text size="large"> Messages </Text>
+                <Text size="large"> My Contacts </Text>
+                <User color="brand" />
+              </Box>
             </Box>
-            <Box gridArea="main" background="" style={{ marginLeft: "50px" }}>
-              <Info />
+            <Box
+              responsive
+              direction="row-responsive"
+              align="center"
+              pad={{ top: 'large', left: 'xlarge', right: 'xlarge' }}
+              margin={{ bottom: 'small' }}
+            >
+              <Grid
+                areas={[
+                  { name: 'nav', start: [0, 0], end: [0, 0] },
+                  { name: 'main', start: [1, 0], end: [1, 0] }
+                ]}
+                columns={['medium', 'flex']}
+                rows={['flex']}
+                gap="small"
+              >
+                <Box gridArea="nav" background="">
+                  <Photo />
+                  <Skills />
+                </Box>
+                <Box
+                  gridArea="main"
+                  background=""
+                  style={{ marginLeft: '50px' }}
+                >
+                  <Info />
+                </Box>
+              </Grid>
             </Box>
-          </Grid>
+          </Box>
         )}
       </ResponsiveContext.Consumer>
     </Grommet>
