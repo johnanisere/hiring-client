7bc5a17596a1e26941e9c0161f1e14a1c4ba3c0e
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-22 10:36:04 +0100

    adds tests for inviteHirer reducers and actions

--- src/compositions/Home.js
+++ src/compositions/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
-import Cards from '../components/dashboard/Cards';
+// import Layout from '../components/Layout';
+// import Cards from '../components/dashboard/Cards';
 // import ScheduleInterview from '../components/dashboard/ScheduleInterview';
+import InviteHirer from '../components/InviteHirer';
 export default function App(props) {
   return (
     <div className="App">
+      <InviteHirer />
       {/* <ScheduleInterview /> */}
-      <Layout>
+      {/* <Layout>
         <Cards />
-      </Layout>
+      </Layout> */}
     </div>
   );
 }
