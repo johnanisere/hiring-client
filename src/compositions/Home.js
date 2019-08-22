50e957cb5c6c858ebc629ec18315032ead318b38
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-22 16:20:39 +0100

    removes comments from Home component

--- src/compositions/Home.js
+++ src/compositions/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-// import Layout from '../components/Layout';
-// import Cards from '../components/dashboard/Cards';
-// import ScheduleInterview from '../components/dashboard/ScheduleInterview';
-import InviteHirer from '../components/InviteHirer';
+import Layout from '../components/Layout';
+import Cards from '../components/dashboard/Cards';
+
 export default function App(props) {
   return (
     <div className="App">
-      <InviteHirer />
-      {/* <ScheduleInterview /> */}
-      {/* <Layout>
+      <Layout>
         <Cards />
-      </Layout> */}
+      </Layout>
     </div>
   );
 }
