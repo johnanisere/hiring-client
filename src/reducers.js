0f37be3d79769cdef22385fee83b568b24df208c
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-22 10:12:15 +0100

    modifies combinedReducer to include reducer for inviteHire component

--- src/reducers.js
+++ src/reducers.js
@@ -3,10 +3,12 @@ import user from './components/authentication/user.reducer';
 import authentication from './components/authentication/authentication.reducer';
 import decadevs from './components/dashboard/decadevs-reducers/decadevs.reducer';
 import interviewDetails from './components/dashboard/scheduleInterview/scheduleInterview.reducer';
+import hirer from './components/InviteHirer/inviteHirer.reducer';
 
 export default combineReducers({
   authentication,
   user,
   decadevs,
-  interviewDetails
+  interviewDetails,
+  hirer
 });
