4131ab1ef923bc5a07c5e1f7cbea02aaeaf7ba34
john anisere <johnanisere@gmail.com>
2019-08-23 12:53:16 +0100

    fix

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,17 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const Home = lazy(() => import('./compositions/Home'));
-const Authentication = lazy(() => import('./compositions/authentication'));
-const InviteForm = lazy(() => import('./components/settings/InviteForm'));
-const Background = lazy(() => import('../src/components/profile/Background'));
+const Home = lazy(() => import("./compositions/Home"));
+const Authentication = lazy(() => import("./compositions/authentication"));
+const InviteForm = lazy(() => import("./components/settings/InviteForm"));
+const Profile = lazy(() => import("../src/components/profile/"));
 
 const App = () => (
   <BrowserRouter>
     <Suspense fallback="loading..">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/profile" component={Background} />
+        <Route exact path="/profile" component={Profile} />
         <Route exact path="/invitemail" component={InviteForm} />
         <Route path="/" component={Authentication} />
       </Switch>
