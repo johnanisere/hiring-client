d6a51a64b4e76e377aec3250653975b61e1e73d1
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-26 16:21:50 +0100

    modifies Card component to iclude next component and modal for more info

--- src/components/dashboard/Cards.js
+++ src/components/dashboard/Cards.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { connect } from "react-redux";
-import request from "../../request";
-import { getAllDecadevs } from "./decadevs.action";
-import MoonLoader from "react-spinners/MoonLoader";
-import Dropdown from "../Dropdown";
-import Next from "./Next";
+import React from 'react';
+import { connect } from 'react-redux';
+import request from '../../request';
+import { getAllDecadevs } from './decadevs.action';
+import MoonLoader from 'react-spinners/MoonLoader';
+import Dropdown from '../Dropdown';
+import Next from './Next';
+import MoreInfo from './MoreInfo';
 
-import Card from "./Card";
-
-import { Grommet, Box, Grid, ResponsiveContext } from "grommet";
+import { Grommet, Box, Text, Image, Grid, ResponsiveContext } from 'grommet';
 
 class Cards extends React.Component {
   state = {
     open: false,
-    gender: "All",
+    gender: 'All',
     fetching: false
   };
   componentDidMount() {
@@ -38,7 +37,7 @@ class Cards extends React.Component {
     return (
       <>
         <Dropdown handleChange={this.handleChange} />
-        <Grommet style={{ overflow: "scroll", minHeight: "100%" }}>
+        <Grommet style={{ overflow: 'scroll', minHeight: '100%' }}>
           {loading && (
             <Box fill width="medium" align="center" justify="center">
               <MoonLoader size={30} />
@@ -49,33 +48,61 @@ class Cards extends React.Component {
             {size => (
               <Grid
                 columns={
-                  size === "small"
-                    ? ["1"]
-                    : size === "medium"
-                    ? ["1/4", "1/4", "1/4", "1/4"]
-                    : size === "large"
-                    ? ["1/4", "1/4", "1/4", "1/4"]
-                    : size === "xlarge"
-                    ? ["1/5", "1/5", "1/5", "1/5", "1/5"]
-                    : ["1/6", "1/6", "1/6", "1/6", "1/6", "1/6"]
+                  size === 'small'
+                    ? ['1']
+                    : size === 'medium'
+                    ? ['1/4', '1/4', '1/4', '1/4']
+                    : size === 'large'
+                    ? ['1/4', '1/4', '1/4', '1/4']
+                    : size === 'xlarge'
+                    ? ['1/5', '1/5', '1/5', '1/5', '1/5']
+                    : ['1/6', '1/6', '1/6', '1/6', '1/6', '1/6']
                 }
               >
-                {!loading &&
-                  decadevs.map((dev, key) => (
-                    <Card
-                      key={key}
-                      dev={dev}
-                      open={this.state.open}
-                      onToggle={this.onToggle}
-                    />
-                  ))}
+                {decadevs.map(dev => {
+                  return (
+                    <Box
+                      key={dev._id}
+                      pad="medium"
+                      align="center"
+                      background={{
+                        color: 'light-2',
+                        opacity: 'strong'
+                      }}
+                      round
+                      gap="small"
+                      margin="medium"
+                    >
+                      <Image
+                        style={{
+                          width: '100%',
+                          height: 'auto',
+                          borderRadius: '12px'
+                        }}
+                        fit="cover"
+                        src={dev.profilePhoto}
+                      />
+                      <Text>{dev.email}</Text>
+                      <MoreInfo
+                        email={dev.email}
+                        profilePhoto={dev.profilePhoto}
+                        phone={dev.phone}
+                        cv={dev.cv}
+                        bio={dev.bio}
+                        name={dev.name}
+                        open={this.state.open}
+                        onToggle={this.onToggle}
+                      />
+                    </Box>
+                  );
+                })}
               </Grid>
             )}
           </ResponsiveContext.Consumer>
           {!loading && (
             <Next
               handleNext={this.handleNext}
-              style={{ borderRadius: "5px" }}
+              style={{ borderRadius: '5px' }}
             />
           )}
         </Grommet>
