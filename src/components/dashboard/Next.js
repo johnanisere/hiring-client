0866de59c19afdd99e3c54d80953360f42245641
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-26 13:24:07 +0100

    Adds Next document to view next set of  decadevs

---
+++ src/components/dashboard/Next.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { grommet, Box, Button, Grommet } from 'grommet';
+
+export default function Next(props) {
+  return (
+    <Grommet theme={grommet}>
+      <Box align="center" pad="medium" margin="large">
+        <Button
+          primary
+          label="Next"
+          onClick={props.handleNext}
+          {...props}
+          color="dark-1"
+        />
+      </Box>
+    </Grommet>
+  );
+}
