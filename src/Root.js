cd899b7633d493e5b7f840d5c1b2bd7e27def726
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-26 10:58:15 +0100

    Created dev profile modal

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,13 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Home = lazy(() => import("./compositions/Home"));
-const Authentication = lazy(() => import("./compositions/authentication"));
-const InviteForm = lazy(() => import("./components/settings/InviteForm"));
-const Background = lazy(() => import("../src/components/profile/Background"));
+const Home = lazy(() => import('./compositions/Home'));
+const Authentication = lazy(() => import('./compositions/authentication'));
+const InviteForm = lazy(() => import('./components/settings/InviteForm'));
+const Background = lazy(() => import('../src/components/profile/Background'));
+const DecadevModal = lazy(() =>
+  import('../src/components/decadevModal/DecadevModal')
+);
 
 const App = () => (
   <BrowserRouter>
@@ -12,6 +15,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/profile" component={Background} />
+        <Route exact path="/viewprofile" component={DecadevModal} />
         <Route exact path="/invitemail" component={InviteForm} />
         <Route path="/" component={Authentication} />
       </Switch>
