94057aad9405543d6018ea118e40fa9afd2ca410
EstherSeyi <sheyiogundijo@gmail.com>
2019-08-30 07:18:25 +0100

    last changes

--- src/components/decadevModal/ModalInfo.js
+++ src/components/decadevModal/ModalInfo.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { getDetails } from '../profile/getDetails';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Heading, Text, Box } from 'grommet';
 import { Phone, Chat, Calendar } from 'grommet-icons';
 
-export default function Info(props) {
+function Info(props) {
   const { email, phone, cv, name } = props;
   const [values, setValues] = useState([]);
-  console.log('NAME: ', name);
+  console.log(props.history);
+  const handleClick = () =>
+    props.history.push(`/schedule-interview?email=${email}`);
 
   useEffect(() => {
     const obj = getDetails();
@@ -97,6 +99,7 @@ export default function Info(props) {
               padding: '8px',
               cursor: 'pointer'
             }}
+            onClick={handleClick}
           >
             {<Calendar size="small" style={{ marginRight: '5px' }} />}
             Schedule Interview
@@ -130,3 +133,5 @@ export default function Info(props) {
     </Box>
   );
 }
+
+export default withRouter(Info);
