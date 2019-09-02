5082e61cf6c8b726ad5a12244d571e039c02456f
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-02 11:22:01 +0100

    adds animation

--- src/components/dashboard/Cards.js
+++ src/components/dashboard/Cards.js
@@ -71,6 +71,7 @@ class Cards extends React.Component {
                       round
                       gap="small"
                       margin="medium"
+                      animation={['fadeIn']}
                     >
                       <Image
                         style={{
