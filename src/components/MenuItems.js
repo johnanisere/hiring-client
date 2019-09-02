e3f17e4cfb4aeb6fca2f7c98928c4d480404179a
seun jay <seunjay92@gmail.com>
2019-09-02 09:24:06 +0100

    bring in withRouter and add onClick handler

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Box, Button, Text } from 'grommet';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Box, Button, Text } from "grommet";
 
-export default function MenuItems(props) {
+function MenuItems(props) {
   return (
     <>
-      <Button key={props.name} href="#" hoverIndicator>
+      <Button
+        key={props.name}
+        hoverIndicator
+        onClick={() => props.history.push("/dashboard")}
+      >
         <Box
           pad={{
-            horizontal: 'medium',
-            vertical: 'small'
+            horizontal: "medium",
+            vertical: "small"
           }}
         >
           <Text>{props.name}</Text>
@@ -17,3 +22,5 @@ export default function MenuItems(props) {
     </>
   );
 }
+
+export default withRouter(MenuItems);
