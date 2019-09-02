a9ee95fcaa8ec35448293d4e7786939881fd2118
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-02 13:07:58 +0100

    double quotes to single quotes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
-import { Box } from "grommet";
-import MenuItems from "./MenuItems";
-import CollapsibleMenu from "./Collapsable";
-import { useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { Box } from 'grommet';
+import MenuItems from './MenuItems';
+import CollapsibleMenu from './Collapsable';
+import { useSelector } from 'react-redux';
 import {
   hiringPartnerMenuItems,
   adminMenuItems
-} from "../components/constants/menu-items";
+} from '../components/constants/menu-items';
 
 export default function SideBar({ sidebar }) {
   const { role } = useSelector(({ user }) => user.data);
   const list =
-    role === "hiringpartner" ? hiringPartnerMenuItems : adminMenuItems;
+    role === 'hiringpartner' ? hiringPartnerMenuItems : adminMenuItems;
   useEffect(() => {
-    console.log( list );
+    console.log(list);
   });
   return (
     <>
@@ -23,16 +23,16 @@ export default function SideBar({ sidebar }) {
           background="dark-3"
           width="small"
           animation={[
-            { type: "fadeIn", duration: 300 },
+            { type: 'fadeIn', duration: 300 },
             {
-              type: "slideRight",
-              size: "xlarge",
+              type: 'slideRight',
+              size: 'xlarge',
               duration: 150
             }
           ]}
         >
           {list.map(value =>
-            typeof value === "string" ? (
+            typeof value === 'string' ? (
               <MenuItems name={value} key={value} />
             ) : (
               <CollapsibleMenu
