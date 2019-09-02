203493556383ffe349fc9c4661c202b58ab5f08a
seun jay <seunjay92@gmail.com>
2019-09-02 09:21:31 +0100

    make lil change

--- src/Root.js
+++ src/Root.js
@@ -1,21 +1,25 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Dashboard = lazy(() => import("./compositions/dashboard"));
-const Authentication = lazy(() => import("./compositions/authentication"));
+const Dashboard = lazy(() => import('./compositions/dashboard'));
+const Authentication = lazy(() => import('./compositions/authentication'));
 
-const Profile = lazy(() => import("./components/profile/"));
+const Profile = lazy(() => import('./components/profile/'));
 const ScheduleInterview = lazy(() =>
-  import("./components/dashboard/ScheduleInterview")
+  import('./components/dashboard/ScheduleInterview')
 );
+const DecadevModal = lazy(() => import('./components/decadevModal/index'));
 
 const App = () => (
   <BrowserRouter>
     <Suspense fallback="loading..">
       <Switch>
+        <Route path="/dashboard" component={Dashboard} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/viewprofile" component={DecadevModal} />
         <Route exact path="/schedule-interview" component={ScheduleInterview} />
+
         <Route path="/" component={Authentication} />
       </Switch>
     </Suspense>
