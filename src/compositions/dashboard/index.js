4e34e8da6aff7ead81ee5d360f86ca4f40f206d1
seun jay <seunjay92@gmail.com>
2019-09-02 09:27:59 +0100

    make changes

--- src/compositions/dashboard/index.js
+++ src/compositions/dashboard/index.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { lazy } from 'react';
 
-import Layout from "../../components/Layout";
-import Cards from "../../components/dashboard/Cards";
-import ProtectedRoute from "../../components/ProtectedRoute";
-import Profile from "../../components/profile";
-import { useSelector } from "react-redux";
+import { Route, Switch } from 'react-router-dom';
+import Layout from '../../components/Layout';
+import ProtectedRoute from '../../components/ProtectedRoute';
+import Profile from '../../components/profile';
+import { useSelector } from 'react-redux';
 
-export default function App(props) {
+const Cards = lazy(() => import('../../components/dashboard/Cards'));
+const InviteForm = lazy(() => import('../../components/settings/InviteForm'));
+const InviteHirer = lazy(() => import('../../components/InviteHirer'));
+
+export default function App({ match }) {
   const { role } = useSelector(({ user }) => user.data);
   return (
     <ProtectedRoute>
-      {role === "dev" ? (
+      {role === 'dev' ? (
         <Profile />
       ) : (
         <Layout>
-          <Cards />
+          <Switch>
+            <Route exact path={`${match.path}`} component={Cards} />
+            <Route
+              exact
+              path={`${match.path}/usermanagement/dev`}
+              component={InviteForm}
+            />
+            <Route
+              exact
+              path={`${match.path}/usermanagement/hiringpartner`}
+              component={InviteHirer}
+            />
+          </Switch>
         </Layout>
       )}
     </ProtectedRoute>
