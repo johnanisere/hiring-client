e1ed7de775797c4a69d77f89d29b4d41dd557864
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-02 13:07:58 +0100

    double quotes to single quotes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from 'grommet';
 import MenuItems from './MenuItems';
 import CollapsibleMenu from './Collapsable';
