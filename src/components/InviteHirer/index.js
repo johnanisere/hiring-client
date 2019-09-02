3941229f5437b3857950d90a1755b54d6c6443e9
seun jay <seunjay92@gmail.com>
2019-09-02 09:25:37 +0100

    make lil change

--- src/components/InviteHirer/index.js
+++ src/components/InviteHirer/index.js
@@ -3,7 +3,7 @@ import { useSelector, connect } from "react-redux";
 import { Box, Button, Form, Text } from "grommet";
 import BeatLoader from "react-spinners/BeatLoader";
 import Input from "../input/index";
-
+import FormLayout from "../FormLayout";
 import FormError from "../formError";
 import inviteHirerBoundActionCreator from "../InviteHirer/inviteHirer.action";
 import request from "../../request";
@@ -41,98 +41,96 @@ function InviteHirer(props) {
   };
 
   return (
-    <>
-      <Box fill align="center" justify="center">
-        <Text
-          width="auto"
-          size="large"
-          margin="small"
-          style={{
-            fontWeight: "bold",
-            fontSize: "25px"
-          }}
-        >
-          Invite Hirer
-        </Text>
-        <Box width="large">
-          <FormError error={error} />
-          <Form onSubmit={handleSubmit}>
-            <Input
-              label="Name"
-              name="name"
-              value={name}
-              required
-              validate={{ regexp: /^[a-z]/i }}
-              onChange={handleChange}
+    <Box fill align="center" justify="center">
+      <Text
+        width="auto"
+        size="large"
+        margin="small"
+        style={{
+          fontWeight: "bold",
+          fontSize: "25px"
+        }}
+      >
+        Invite Hiring Partner
+      </Text>
+      <Box width="large">
+        <FormError error={error} />
+        <Form onSubmit={handleSubmit}>
+          <Input
+            label="Name"
+            name="name"
+            value={name}
+            required
+            validate={{ regexp: /^[a-z]/i }}
+            onChange={handleChange}
+          />
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            value={email}
+            required
+            onChange={handleChange}
+          />
+          <Input
+            label="Phone"
+            name="phone"
+            type="phone"
+            value={phone}
+            required
+            onChange={handleChange}
+          />
+          <Input
+            label="Contact Person"
+            name="contactPerson"
+            type="contactPerson"
+            value={contactPerson}
+            required
+            onChange={handleChange}
+          />
+          <Input
+            label="Company URL"
+            name="companyURL"
+            type="companyURL"
+            value={companyURL}
+            required
+            onChange={handleChange}
+          />
+          <Input
+            label="Password"
+            name="password"
+            required
+            value={password}
+            type="password"
+            validate={{
+              regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+              message:
+                "Password must contain at least 8 characters, 1 letter, and 1 number"
+            }}
+            onChange={handleChange}
+          />
+          <Input
+            label="Address"
+            name="address"
+            required
+            value={address}
+            onChange={handleChange}
+          />
+          <Box direction="row" justify="between" margin={{ top: "medium" }}>
+            <Button
+              primary
+              width="large"
+              color="dark-1"
+              label={
+                loading ? <BeatLoader size={5} color="#fff" /> : "Send Invite"
+              }
+              type="submit"
+              style={{ width: "100%", marginTop: 20 }}
             />
-            <Input
-              label="Email"
-              name="email"
-              type="email"
-              value={email}
-              required
-              onChange={handleChange}
-            />
-            <Input
-              label="Phone"
-              name="phone"
-              type="phone"
-              value={phone}
-              required
-              onChange={handleChange}
-            />
-            <Input
-              label="Contact Person"
-              name="contactPerson"
-              type="contactPerson"
-              value={contactPerson}
-              required
-              onChange={handleChange}
-            />
-            <Input
-              label="Company URL"
-              name="companyURL"
-              type="companyURL"
-              value={companyURL}
-              required
-              onChange={handleChange}
-            />
-            <Input
-              label="Password"
-              name="password"
-              required
-              value={password}
-              type="password"
-              validate={{
-                regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                message:
-                  "Password must contain at least 8 characters, 1 letter, and 1 number"
-              }}
-              onChange={handleChange}
-            />
-            <Input
-              label="Address"
-              name="address"
-              required
-              value={address}
-              onChange={handleChange}
-            />
-            <Box direction="row" justify="between" margin={{ top: "medium" }}>
-              <Button
-                primary
-                width="large"
-                color="dark-1"
-                label={
-                  loading ? <BeatLoader size={5} color="#fff" /> : "Send Invite"
-                }
-                type="submit"
-                style={{ width: "100%", marginTop: 20 }}
-              />
-            </Box>
-          </Form>
-        </Box>
+          </Box>
+        </Form>
       </Box>
-    </>
+    </Box>
   );
 }
 
