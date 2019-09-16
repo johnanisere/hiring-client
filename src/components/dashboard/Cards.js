0ae6ae1e6e8dd36b5a235ffe01909f775f84718a
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-16 12:21:28 +0100

    imports finalize and selectcheck components in cards and integrated them

--- src/components/dashboard/Cards.js
+++ src/components/dashboard/Cards.js
@@ -6,6 +6,8 @@ import MoonLoader from 'react-spinners/MoonLoader';
 import Dropdown from '../Dropdown';
 import Next from './Next';
 import MoreInfo from './MoreInfo';
+import Finalize from '../selected/Finalize';
+import SelectCheck from '../selected/SelectCheck';
 
 import { Grommet, Box, Text, Image, Grid, ResponsiveContext } from 'grommet';
 
@@ -43,7 +45,7 @@ class Cards extends React.Component {
               <MoonLoader size={30} />
             </Box>
           )}
-          <Box align="start" justify="start"></Box>
+          <Finalize />
           <ResponsiveContext.Consumer>
             {size => (
               <Grid
@@ -83,6 +85,7 @@ class Cards extends React.Component {
                         fit="cover"
                         src={dev.profilePhoto}
                       />
+
                       <Text>{dev.email}</Text>
                       <MoreInfo
                         email={dev.email}
@@ -94,6 +97,7 @@ class Cards extends React.Component {
                         open={this.state.open}
                         onToggle={this.onToggle}
                       />
+                      <SelectCheck />
                     </Box>
                   );
                 })}
