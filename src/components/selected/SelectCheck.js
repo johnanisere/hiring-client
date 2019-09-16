05db6853554d44b96314cf2a049a65ad18ea54ac
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-16 12:20:46 +0100

    Adds select check component

--- src/components/selected/SelectCheck.js
+++ src/components/selected/SelectCheck.js
@@ -1,42 +1,9 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Star } from 'grommet-icons';
-import { Box, Text } from 'grommet';
-import selectedBoundActionCreator from './selected.action';
-function SelectCheck(props) {
-  const { selected } = props;
+import React, { useState } from 'react';
+import { CheckBox } from 'grommet';
 
-  const onChange = () => {
-    props.addToSelected(props.decadevObject, !selected);
-  };
+export default function SelectCheck(props) {
+  const [checked, setChecked] = useState(!!props.checked);
+  const onChange = event => setChecked(event.target.checked);
 
-  return (
-    <Box
-      as="span"
-      style={{
-        display: 'block',
-        position: 'absolute',
-        cursor: 'pointer',
-        zIndex: 5,
-        bottom: '100px',
-        left: '20px',
-        ...props.style
-      }}
-    >
-      <Text color="white" style={{ display: 'flex', alignItems: 'center' }}>
-        <Star onClick={onChange} color={selected ? 'red' : 'white'} />
-      </Text>
-    </Box>
-  );
+  return <CheckBox {...props} checked={checked} onChange={onChange} />;
 }
-SelectCheck.defaultProps = {
-  style: {}
-};
-const mapDispatchToProps = {
-  addToSelected: selectedBoundActionCreator
-};
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(SelectCheck);
