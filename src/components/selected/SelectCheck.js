811ed1ab57c895822f65246472b2f6f3f8df9735
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-18 13:05:24 +0100

    modifies SelectCheck.js

--- src/components/selected/SelectCheck.js
+++ src/components/selected/SelectCheck.js
@@ -1,9 +1,35 @@
 import React, { useState } from 'react';
 import { CheckBox } from 'grommet';
+import { connect } from 'react-redux';
+import selectedBoundActionCreator from './selected.action';
 
-export default function SelectCheck(props) {
+function SelectCheck(props) {
   const [checked, setChecked] = useState(!!props.checked);
-  const onChange = event => setChecked(event.target.checked);
+  console.log('CHECKED1', checked);
+  const onChange = event => {
+    setChecked(event.target.checked);
+    console.log('CHECKED2', checked);
+  };
+  if (checked) {
+    props.addToSelected(props.decadevObject);
+  }
 
-  return <CheckBox {...props} checked={checked} onChange={onChange} />;
+  console.log('CHECKED3', checked);
+
+  return (
+    <CheckBox
+      {...props}
+      checked={checked}
+      onChange={event => onChange(event)}
+    />
+  );
 }
+
+const mapDispatchToProps = {
+  addToSelected: selectedBoundActionCreator
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SelectCheck);
