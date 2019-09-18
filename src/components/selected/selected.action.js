4aabfd1064e235fdf4af84343e34ecc7d3549d79
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-18 13:06:11 +0100

    adds action creator for Selected

--- src/components/selected/selected.action.js
+++ src/components/selected/selected.action.js
@@ -1,25 +1,14 @@
 export const ADD_TO_SELECTED = 'ADD_TO_SELECTED';
-export const REMOVE_FROM_SELECTED = 'REMOVE_FROM_SELECTED';
 
-export const addToSelected = payload => {
+export const addToSelected = decadevObject => {
   return {
     type: ADD_TO_SELECTED,
-    payload
+    decadevObject
   };
 };
 
-export const removeFromSelected = payload => {
-  return {
-    type: REMOVE_FROM_SELECTED,
-    payload
-  };
-};
-
-const selectedBoundActionCreator = (decadevObject, selected) => dispatch => {
-  const action = !selected
-    ? removeFromSelected(decadevObject)
-    : addToSelected(decadevObject);
-  dispatch(action);
+const selectedBoundActionCreator = decadevObject => dispatch => {
+  dispatch(addToSelected(decadevObject));
 };
 
 export default selectedBoundActionCreator;
