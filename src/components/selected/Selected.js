984322c35e5b6289508d57cbf4e0cbcbe5768e9a
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-18 13:06:23 +0100

    Adds Selected Component

---
+++ src/components/selected/Selected.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Selected() {
+  return (
+    <div>
+      <h1>Selected</h1>
+    </div>
+  );
+}
+
+export default connect(
+  null,
+  null
+)(Selected);
