12ff02d958a2b00d864dc8dbfd9c501c3cb030fd
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-18 12:57:56 +0100

    adds selectedReducer to root reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,16 +3,14 @@ import user from './components/authentication/user.reducer';
 import authentication from './components/authentication/authentication.reducer';
 import decadevs from './components/dashboard/decadevs.reducer';
 import interviewDetails from './components/dashboard/scheduleInterview.reducer';
-import hirer from './components/HirerSignUp/hirerSignup.reducer';
-import shortlisted from './components/selected/selected.reducer';
-import inactiveHirer from './components/activateHirer/activateHirer.reducer';
+import hirer from './components/InviteHirer/inviteHirer.reducer';
+import selectedReducer from './components/selected/selected.reducer';
 
 export default combineReducers({
   user,
   hirer,
   decadevs,
-  shortlisted,
-  authentication,
   interviewDetails,
-  inactiveHirer
+  hirer,
+  selectedReducer
 });
