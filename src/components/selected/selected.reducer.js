170297181f92c7b39da2a5eb6a737eb998ed87fa
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-18 13:06:43 +0100

    adds selected reducer

---
+++ src/components/selected/selected.reducer.js
@@ -0,0 +1,20 @@
+import { ADD_TO_SELECTED } from './selected.action';
+
+const initialState = {
+  selectedDecadevs: [],
+  total: 0
+};
+
+export default function selectedReducer(state = initialState, action) {
+  let addedDecadev = action.decadevObject;
+  switch (action.type) {
+    case ADD_TO_SELECTED:
+      return {
+        ...state,
+        selectedDecadevs: [...state.selectedDecadevs, addedDecadev],
+        total: state.total++
+      };
+    default:
+      return state;
+  }
+}
