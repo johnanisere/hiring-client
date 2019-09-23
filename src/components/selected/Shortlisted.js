ae6840cd0db8599aa5821f7dd9f1dc0a24d25fd5
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 11:06:57 +0100

    renamed selected component to be Shortlisted

--- src/components/selected/Shortlisted.js
+++ src/components/selected/Shortlisted.js
@@ -1,50 +1,15 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import Card from "../dashboard/Card";
-
-import { Grommet, Grid, ResponsiveContext } from "grommet";
-
-export default function Shortlisted() {
-  const { selectedDecadevs } = useSelector(({ shortlisted }) => shortlisted);
-  const shortlistedDevs = Object.values(selectedDecadevs);
-  const [state, setState] = useState({
-    open: false,
-    gender: "All"
-  });
-
-  const onToggle = () => setState({ ...state, open: !state.open });
-
+function Selected() {
   return (
-    <>
-      <Grommet style={{ overflow: "scroll", minHeight: "100%" }}>
-        <ResponsiveContext.Consumer>
-          {size => (
-            <Grid
-              columns={
-                size === "small"
-                  ? ["1"]
-                  : size === "medium"
-                  ? ["1/4", "1/4", "1/4", "1/4"]
-                  : size === "large"
-                  ? ["1/4", "1/4", "1/4", "1/4"]
-                  : size === "xlarge"
-                  ? ["1/5", "1/5", "1/5", "1/5", "1/5"]
-                  : ["1/6", "1/6", "1/6", "1/6", "1/6", "1/6"]
-              }
-            >
-              {shortlistedDevs.map((dev, key) => (
-                <Card
-                  key={key}
-                  dev={dev}
-                  open={state.open}
-                  onToggle={onToggle}
-                />
-              ))}
-            </Grid>
-          )}
-        </ResponsiveContext.Consumer>
-      </Grommet>
-    </>
+    <div>
+      <h1>Selected</h1>
+    </div>
   );
 }
+
+export default connect(
+  null,
+  null
+)(Selected);
