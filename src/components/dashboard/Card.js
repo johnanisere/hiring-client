fae5fcd142abe8122e34a591e6b905eacad81e89
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 10:45:45 +0100

    removes redundant code

--- src/components/dashboard/Card.js
+++ src/components/dashboard/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import MoreInfo from './MoreInfo';
 import SelectCheck from '../selected/SelectCheck';
 import { useSelector } from 'react-redux';
