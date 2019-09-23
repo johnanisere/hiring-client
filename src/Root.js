810f351c3544f23d4534d5259ee94068dc73865c
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 13:57:29 +0100

    creates route for shorlisted component

--- src/Root.js
+++ src/Root.js
@@ -8,17 +8,17 @@ const ScheduleInterview = lazy(() =>
   import('./components/dashboard/ScheduleInterview')
 );
 const DecadevModal = lazy(() => import('./components/decadevModal/index'));
+const Shortlisted = lazy(() => import('./components/selected/Shortlisted'));
 
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<Loading />}>
       <Switch>
-        <Route path="/dashboard" component={Dashboard} />
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/dashboard" component={Dashboard} />
         <Route exact path="/viewprofile" component={DecadevModal} />
-        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/schedule-interview" component={ScheduleInterview} />
+        <Route exact path="/shortlisted" component={Shortlisted} />
 
         <Route path="/" component={Authentication} />
       </Switch>
