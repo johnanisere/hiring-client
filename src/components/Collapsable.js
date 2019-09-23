6d5e1ce423561a72d6d2c89b51eca964d88746f8
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 13:57:59 +0100

    removes redundant code

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { Box, Button, Collapsible, Grommet, Text } from 'grommet';
