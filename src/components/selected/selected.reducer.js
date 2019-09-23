e35b0ab3dae8f89ccc68422ad25969783c5f0dfd
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 10:59:56 +0100

    adds REMOVE_FROM_SELECTED reducer

--- src/components/selected/selected.reducer.js
+++ src/components/selected/selected.reducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_SELECTED } from './selected.action';
+import { ADD_TO_SELECTED, REMOVE_FROM_SELECTED } from './selected.action';
 
 const initialState = {
   selectedDecadevs: {},
@@ -6,15 +6,27 @@ const initialState = {
 };
 
 export default function selectedReducer(state = initialState, action) {
-  let addedDecadev = action.decadevObject;
   switch (action.type) {
     case ADD_TO_SELECTED:
       return {
         ...state,
         selectedDecadevs: {
           ...state.selectedDecadevs,
-          ...state.selectedDecadevs,
+          ...{
+            [action.payload.email]: action.payload
+          }
+        },
         total: state.total++
+      };
+    case REMOVE_FROM_SELECTED:
+      const oldshorlisted = { ...state.selectedDecadevs };
+      delete oldshorlisted[action.payload.email];
+      return {
+        ...state,
+        selectedDecadevs: {
+          ...oldshorlisted
+        },
+        total: state.total--
       };
     default:
       return state;
