6725d7d5d8e892ce4796e567d4c84bbee4962543
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 14:01:38 +0100

    modifies finalize button to redirect to shortlisted route when clicked

--- src/components/selected/Finalize.js
+++ src/components/selected/Finalize.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { Button } from 'grommet';
 
 export default function Finalize(props) {
+  const [state, setState] = useState(false);
+  const handleClick = () => setState(true);
+
+  if (state) {
+    return <Redirect to="/dashboard/shortlisted" />;
+  }
+
   return (
     <Grommet theme={grommet}>
       <Box align="center" pad="medium" margin="large">
-        <Button
-          primary
-          label="Finalize"
-          // onClick={props.handleNext}
-          // {...props}
-          color="dark-1"
-        />
+        <Button primary label="Finalize" onClick={handleClick} color="dark-1" />
       </Box>
     </Grommet>
   );
