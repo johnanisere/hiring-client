9d119f3d86d71fad76b4eb9c7960538e7f718b16
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 10:42:59 +0100

    chnages selectedReducer to shortlisted

--- src/reducers.js
+++ src/reducers.js
@@ -4,13 +4,13 @@ import authentication from './components/authentication/authentication.reducer';
 import decadevs from './components/dashboard/decadevs.reducer';
 import interviewDetails from './components/dashboard/scheduleInterview.reducer';
 import hirer from './components/InviteHirer/inviteHirer.reducer';
-import selectedReducer from './components/selected/selected.reducer';
+import shortlisted from './components/selected/selected.reducer';
 
 export default combineReducers({
-  authentication,
   user,
-  decadevs,
-  interviewDetails,
   hirer,
-  selectedReducer
+  decadevs,
+  shortlisted,
+  authentication,
+  interviewDetails
 });
