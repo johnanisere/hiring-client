213c35e78ab65ade80269007b4e0d0dc9d577bdb
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-23 10:54:30 +0100

    includes a removeFromSelected action to cater for unchecking of card checkbox

--- src/components/selected/selected.action.js
+++ src/components/selected/selected.action.js
@@ -1,14 +1,25 @@
 export const ADD_TO_SELECTED = 'ADD_TO_SELECTED';
+export const REMOVE_FROM_SELECTED = 'REMOVE_FROM_SELECTED';
 
-export const addToSelected = decadevObject => {
+export const addToSelected = payload => {
   return {
     type: ADD_TO_SELECTED,
-    decadevObject
+    payload
   };
 };
 
-const selectedBoundActionCreator = decadevObject => dispatch => {
-  dispatch(addToSelected(decadevObject));
+export const removeFromSelected = payload => {
+  return {
+    type: REMOVE_FROM_SELECTED,
+    payload
+  };
+};
+
+const selectedBoundActionCreator = (decadevObject, selected) => dispatch => {
+  const action = !selected
+    ? removeFromSelected(decadevObject)
+    : addToSelected(decadevObject);
+  dispatch(action);
 };
 
 export default selectedBoundActionCreator;
