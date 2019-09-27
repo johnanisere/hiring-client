d27cd1f7172e3bb17220867eb1b920a220f44d50
john anisere <johnanisere@gmail.com>
2019-09-27 12:26:42 +0100

    fix

--- src/components/profil/index.js
+++ src/components/profil/index.js
@@ -3,10 +3,12 @@ import Summary from './Summary';
 import Portfolio from './Portfolio';
 import Employment from './Employment';
 import Skills from './Skills';
+import Action from './Action';
 
-const App = ({ skills }) => (
+const App = props => (
   <>
     <Summary />
+    <Action dev={props.dev} selected={props.selected} />
     <Portfolio />
     <Employment />
     <Skills />
