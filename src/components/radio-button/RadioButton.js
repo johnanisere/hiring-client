5cdf244ee6be243b74738880a21065844ac82ce9
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-27 13:39:41 +0100

    adds radio button for multiple choice questions in hirersignup componenet

---
+++ src/components/radio-button/RadioButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Grommet, RadioButton, Text } from 'grommet';
+import { grommet } from 'grommet/themes';
+
+export default function MultipleChoices({ random, ...rest }) {
+  return (
+    <Grommet theme={grommet}>
+      <Box
+        pad={{
+          top: 'small',
+          bottom: 'small'
+        }}
+        gap="small"
+      >
+        <Text pad="none">{rest.question}</Text>
+        {rest.choices.map(choice => {
+          return (
+            <RadioButton
+              key={choice}
+              label={choice}
+              name={rest.name}
+              value={choice}
+              checked={choice === random}
+              onChange={rest.changed}
+              {...rest}
+            />
+          );
+        })}
+      </Box>
+    </Grommet>
+  );
+}
