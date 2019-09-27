6e065c3666126749d3fa3de002c9db1add1c48d2
EstherSeyi <sheyiogundijo@gmail.com>
2019-09-27 13:38:53 +0100

    adds verify hirer component

---
+++ src/components/HirerSignUp/VerifyHirer.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import request from '../../request';
+
+export default function VerifyHirer({ match }) {
+  const {
+    params: { token, email }
+  } = match;
+
+  // eslint-disable-next-line
+  const [state, setState] = useState({ token, email });
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    request
+      .put('http://localhost:3005/api/v1/hirer/verifyhirer', { ...state })
+      .then(res => {
+        setMessage(res.data.message);
+      });
+  }, [state, message]);
+
+  return <h1>{message}</h1>;
+}
