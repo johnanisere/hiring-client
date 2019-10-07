4a7c18afac3c7815497a6fb02da5ea61be53b347
EstherSeyi <sheyiogundijo@gmail.com>
2019-10-07 10:54:22 +0100

    adds hirer login feature

--- src/components/HirerSignUp/hirerSignup.reducer.js
+++ src/components/HirerSignUp/hirerSignup.reducer.js
@@ -1,11 +1,19 @@
 import { REGISTER_HIRER } from './hirerSignup.action';
+import { HIRER_LOGIN } from './hirerLogin/hirerLogin.action';
 
 export const initialState = {
-  hirer: {}
+  hirer: {},
+  loading: false,
+  error: {}
 };
 
 export default function hirer(state = initialState, action) {
   switch (action.type) {
+    case HIRER_LOGIN:
+      return {
+        ...state,
+        hirer: action.payload
+      };
     case REGISTER_HIRER:
       return {
         ...state,
@@ -15,6 +23,11 @@ export default function hirer(state = initialState, action) {
       return {
         ...state,
         loading: action.payload
+      };
+    case 'HIRER_LOGIN_ERROR':
+      return {
+        ...state,
+        error: action.payload
       };
     case 'REGISTER_HIRER_ERROR':
       return {
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import MoonLoader from 'react-spinners/MoonLoader';
-import { Box } from 'grommet';
+import { Box, Grommet } from 'grommet';
+import { grommet } from 'grommet/themes';
 const Loading = () => (
-  <Box fill width="medium" align="center" justify="center">
-    <MoonLoader size={30} />
-  </Box>
+  <Grommet full theme={grommet}>
+    <Box fill align="center" justify="center">
+      <Box fill width="medium" align="center" justify="center">
+        <MoonLoader size={30} />
+      </Box>
+    </Box>
+  </Grommet>
 );
 
 export default Loading;
