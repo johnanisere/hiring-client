8cbfb0d8e0cf8662e625f7ce1a789d30c8d2cdbd
EstherSeyi <sheyiogundijo@gmail.com>
2019-10-11 08:19:37 +0100

    rebase with develop

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
 
 const Dashboard = lazy(() => import('./compositions/dashboard'));
 const Authentication = lazy(() => import('./compositions/authentication'));
@@ -8,7 +9,6 @@ const ScheduleInterview = lazy(() =>
   import('./components/dashboard/ScheduleInterview')
 );
 const DecadevModal = lazy(() => import('./components/decadevModal/index'));
-const Shortlisted = lazy(() => import('./components/selected/Shortlisted'));
 
 const App = () => (
   <BrowserRouter>
