ce1bd9b8c4ec6402d70e91c25a733ecb45ecf1a8
EstherSeyi <sheyiogundijo@gmail.com>
2019-11-06 15:23:22 +0100

    persists app data with redux persists

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Root from './Root';
 import { Provider } from 'react-redux';
 import ErrorBoundary from './ErrorBoundary';
-import store from './store';
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './App.css';
 const App = () => (
   <ErrorBoundary>
     <Provider store={store}>
-      <Root />
+      <PersistGate loading={null} persistor={persistor}>
+        <Root />
+      </PersistGate>
     </Provider>
   </ErrorBoundary>
 );
--- src/store.js
+++ src/store.js
@@ -2,9 +2,19 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: hardSet
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = applyMiddleware(thunk);
 const enhancer = composeWithDevTools(middleware);
-const store = createStore(rootReducer, enhancer);
 
+let store = createStore(persistedReducer, enhancer);
+export let persistor = persistStore(store);
 export default store;
