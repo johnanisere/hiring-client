66934c8a22c29054c5751e3e2cc854d27c3eec07
EstherSeyi <sheyiogundijo@gmail.com>
2019-11-11 16:11:19 +0100

    adds routes for the new tables

--- src/compositions/dashboard/index.js
+++ src/compositions/dashboard/index.js
@@ -12,6 +12,12 @@ const Shortlisted = lazy(() => import('../../components/selected/Shortlisted'));
 const AllInterviews = lazy(() =>
   import('../../components/interviewActivities/AllInterviews')
 );
+const DeclinedInterviewsTable = lazy(() =>
+  import('../../components/interviewActivities/DeclinedInterviews')
+);
+const AcceptedInterviewsTable = lazy(() =>
+  import('../../components/interviewActivities/AcceptedInterviews')
+);
 
 export default function App({ match }) {
   const { role } = useSelector(({ user }) => user.data);
@@ -42,6 +48,16 @@ export default function App({ match }) {
               exact
               path={`${match.path}/interviews/scheduledinterviews`}
               component={AllInterviews}
+            />
+            <Route
+              exact
+              path={`${match.path}/interviews/declinedinterviews`}
+              component={DeclinedInterviewsTable}
+            />
+            <Route
+              exact
+              path={`${match.path}/interviews/acceptedinterviews`}
+              component={AcceptedInterviewsTable}
             />
           </Switch>
         </Layout>
