c0ce9c899598861fc225c5ecbde9d8354b142ad2
EstherSeyi <sheyiogundijo@gmail.com>
2019-11-18 11:10:18 +0100

    modifies update password feature for decadev

--- src/components/authentication/UpdatePassword.js
+++ src/components/authentication/UpdatePassword.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
-import { BeatLoader } from 'react-spinners';
-import { Box, Button, Form, Text, FormField } from 'grommet';
+import Button from '../button/FormButton';
+import { Box, Form, Text } from 'grommet';
 import request from '../../request';
 import updatePasswordBoundActionCreator from './updatePassword.action';
-import SuccessNotification from '../toasters/SuccessNotification';
 import FormError from '../formError';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
+import SuccessNotification from '../toasters/SuccessNotification';
+import Input from '../input';
+import Formlayout from '../FormLayout';
 
 function UpdatePassword({ onUpdatePassword, match }) {
+  const { error } = useSelector(({ error }) => error);
+  const [loading, onLoading] = useState(false);
+
+  const [success, onSuccess] = useState('');
   const [values, setValues] = useState({
     password: '',
     confirmPassword: ''
   });
-  const [error, onError] = useState({});
-  const [loading, onLoading] = useState(false);
-  const [success, onSuccess] = useState('');
-  const [diffPassword, setDiffPassword] = useState(false);
 
   const {
-    params: { token }
+    params: { token, email }
   } = match;
   const { password, confirmPassword } = values;
 
@@ -26,12 +28,11 @@ function UpdatePassword({ onUpdatePassword, match }) {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
   };
+
   const activityIndicator = value => {
     onLoading(value);
   };
-  const handleError = error => {
-    onError(error);
-  };
+
   const handleSuccess = val => {
     onSuccess(val);
   };
@@ -39,17 +40,11 @@ function UpdatePassword({ onUpdatePassword, match }) {
   const handleSubmit = e => {
     e.preventDefault();
 
-    const condition = password !== confirmPassword;
-    if (condition) {
-      setDiffPassword(true);
-      return;
-    }
     onUpdatePassword(
-      { newPassword: confirmPassword },
+      { password, confirmPassword, email },
       request,
       token,
       activityIndicator,
-      handleError,
       handleSuccess
     );
   };
@@ -60,83 +55,55 @@ function UpdatePassword({ onUpdatePassword, match }) {
       {success && (
         <SuccessNotification message={success.message} onClose={closeToaster} />
       )}
-      <Box margin="small" pad="small">
-        <Text
-          width="auto"
-          size="large"
-          margin="small"
-          style={{
-            fontWeight: 'bold',
-            fontSize: '25px'
-          }}
-        >
-          Update Password
-        </Text>
-        <Text size="small" alignSelf="center">
-          Kindly update your password here
-        </Text>
-      </Box>
-      {diffPassword && (
-        <small className="error" style={{ color: 'red', textAlign: 'center' }}>
-          Make sure passwords on both fields are the same!
-        </small>
-      )}
-      <FormError error={error} />
-      <Form onSubmit={e => handleSubmit(e)}>
-        <FormField
-          name="password"
-          required
-          validate={{
-            regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-            message:
-              'Password must contain at least 8 characters, 1 letter, and 1 number'
-          }}
-          value={password}
-          onChange={handleChange}
-          placeholder="Password"
-          type="password"
-          style={{
-            marginBottom: '15px',
-            borderRadius: '20px'
-          }}
-        />
-        <FormField
-          placeholder="Confirm Password"
-          name="confirmPassword"
-          type="password"
-          required
-          value={confirmPassword}
-          onChange={handleChange}
-          validate={{
-            regexp: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-            message:
-              'Password must contain at least 8 characters, 1 letter, and 1 number'
-          }}
-          color="dark-1"
-          style={{
-            marginBottom: '15px',
-            borderRadius: '20px'
-          }}
-        />
-
-        <Box
-          direction="row"
-          justify="center"
-          align="center"
-          margin={{ top: 'medium' }}
-        >
-          <Button
-            primary
-            width="large"
-            color="dark-1"
-            label={
-              loading ? <BeatLoader size={5} color="#fff" /> : 'Update Password'
-            }
-            type="submit"
-            style={{ width: '100%', marginTop: 20 }}
-          />
+      <Formlayout>
+        <Box margin="small" pad="small">
+          <Text
+            width="auto"
+            size="large"
+            margin="auto"
+            style={{
+              fontWeight: 'bold',
+              fontSize: '25px',
+              paddingBottom: '15px'
+            }}
+          >
+            Update Password
+          </Text>
+          <Text size="small" alignSelf="center">
+            Create new password
+          </Text>
         </Box>
-      </Form>
+
+        <Form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
+          <FormError error={error} />
+          <Input
+            name="password"
+            required
+            value={password}
+            onChange={handleChange}
+            placeholder="Password"
+            type="password"
+            style={{
+              marginBottom: '15px',
+              borderRadius: '20px'
+            }}
+          />
+          <Input
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            type="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={handleChange}
+            color="dark-1"
+            style={{
+              marginBottom: '15px',
+              borderRadius: '20px'
+            }}
+          />
+          <Button loading={loading} type="submit" text="Update Password" />
+        </Form>
+      </Formlayout>
     </>
   );
 }
@@ -144,7 +111,4 @@ function UpdatePassword({ onUpdatePassword, match }) {
 const mapDispatchToProps = {
   onUpdatePassword: updatePasswordBoundActionCreator
 };
-export default connect(
-  null,
-  mapDispatchToProps
-)(UpdatePassword);
+export default connect(null, mapDispatchToProps)(UpdatePassword);
