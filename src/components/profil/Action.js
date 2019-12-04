e4e9b3250a82d11bf897514e1fc00cbc02e99b13
EstherSeyi <sheyiogundijo@gmail.com>
2019-12-04 12:44:26 +0100

    adds feature to removes scheduleinterview button if interview has already been scheduled

--- src/components/profil/Action.js
+++ src/components/profil/Action.js
@@ -1,54 +1,77 @@
 import React from "react";
 import Profile from "./blocks";
 import SelectCheck from "../selected/SelectCheck";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-const App = ({ selected, dev }) => (
-  <>
-    <Profile
-      bottom
-      style={{
-        width: "calc(100% - 50px)",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, .09)"
-      }}
-    >
-      <Profile.Tag style={{ marginRight: "10px" }} blue medium>
-        <Link
-          to={`/schedule-interview/${dev.email}`}
-          style={{ textDecoration: "none", color: "white" }}
-        >
-          Schedule interview
-        </Link>
-      </Profile.Tag>
 
-      <Profile.Tag style={{ marginRight: "10px" }} blue medium>
-        <Link
-          to={`/schedule-test/${dev.email}`}
-          style={{ textDecoration: "none", color: "white" }}
-        >
-          Schedule Test
-        </Link>
-      </Profile.Tag>
-      {"   "}
-      <Profile.Text style={{ marginRight: "10px" }} bold size2>
-        OR
-      </Profile.Text>
-      <Profile.Text
-        bold
-        size6
-        style={{ position: "relative", display: "flex", alignItems: "center" }}
+const App = ({ selected, dev }) => {
+  const { interviewDetails, testDetails  } = useSelector(
+    ({ interviewDetails}) => interviewDetails
+  );
+
+  console.log({testDetails})
+
+  return (
+    <>
+      <Profile
+        bottom
+        style={{
+          width: "calc(100% - 50px)",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, .09)"
+        }}
       >
-        <SelectCheck
-          decadevObject={dev}
-          selected={selected}
-          style={{ position: "initial", marginRight: "5px" }}
-        />
-        Save for later
-      </Profile.Text>
-    </Profile>
-  </>
-);
+        {(interviewDetails.scheduled || testDetails.scheduled) &&
+        interviewDetails.decaDev === dev.email ? (
+          <em style={{ fontWeight: "lighter" }}> 
+            Interview has been scheduled for {interviewDetails.scheduled || testDetails.scheduled}
+          </em>
+        ) : (
+          <>
+            <Profile.Tag style={{ marginRight: "10px" }} blue medium>
+              <Link
+                to={`/schedule-interview/${dev.email}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Schedule interview
+              </Link>
+            </Profile.Tag>
+
+            <Profile.Tag style={{ marginRight: "10px" }} blue medium>
+              <Link
+                to={`/schedule-test/${dev.email}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Schedule Test
+              </Link>
+            </Profile.Tag>
+            <Profile.Text style={{ marginRight: "10px" }} bold size2>
+              OR
+            </Profile.Text>
+        {"   "}
+        <Profile.Text
+          bold
+          size6
+          style={{
+            position: "relative",
+            display: "flex",
+            alignItems: "center"
+          }}
+        >
+          <SelectCheck
+            decadevObject={dev}
+            selected={selected}
+            style={{ position: "initial", marginRight: "5px" }}
+          />
+          Save for later
+        </Profile.Text>
+          </>
+        )}
+      </Profile>
+    </>
+  );
+};
 
 export default App;
