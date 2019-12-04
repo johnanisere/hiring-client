e8f9a9fddfa28b55c6debe21b9712c2cc6382ca5
EstherSeyi <sheyiogundijo@gmail.com>
2019-12-04 22:13:49 +0100

    modifies request

--- src/request.js
+++ src/request.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import store from "./store";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:3005/api/v1"
   baseURL: `${process.env.REACT_APP_BASE_URL}/api/v1`
 });
 
