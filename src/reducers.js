9a9556a6d0542b8bab9a705f135a4a1d55beebde
whitehox <oketegah@gmail.com>
2019-12-11 10:32:05 +0100

    added reducer for next button controll to combined reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import user from './components/authentication/user.reducer';
+import user, { initialState } from './components/authentication/user.reducer';
 import authentication from './components/authentication/authentication.reducer';
 import decadevs from './components/dashboard/decadevs.reducer';
 import interviewDetails from './components/dashboard/scheduleInterview.reducer';
@@ -8,16 +8,16 @@ import shortlisted from './components/selected/selected.reducer';
 import hirer from './components/activateHirer/activateHirer.reducer';
 import interviews from './components/interviewActivities/interviews.reducer';
 import error from './components/authentication/error.reducer';
-import hired from './components/hired/hired.reducer';
+import allowNext from './components/AllowNext/allownext.reducer';
 
 export default combineReducers({
-	user,
-	error,
-	decadevs,
-	shortlisted,
-	authentication,
-	interviewDetails,
-	hirer,
-	interviews,
-	hired
+  user,
+  error,
+  decadevs,
+  shortlisted,
+  authentication,
+  interviewDetails,
+  hirer,
+  interviews,
+  allowNext,
 });
