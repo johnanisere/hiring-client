b3bb2f2bab9b067ee0ceb7d0b62f8459dd039565
EstherSeyi <sheyiogundijo@gmail.com>
2019-12-16 10:42:33 +0100

    modifies error messages

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,58 +1,89 @@
-import differenceInCalendarMonths from "date-fns/differenceInCalendarMonths";
-import differenceInCalendarYears from "date-fns/differenceInCalendarYears";
+import differenceInCalendarMonths from 'date-fns/differenceInCalendarMonths';
+import differenceInCalendarYears from 'date-fns/differenceInCalendarYears';
 
 export const errorMessageExtrator = error => {
-  if (
-    error &&
-    error.response &&
-    error.response.data &&
-    error.response.data.error
-  ) {
-    return error.response.data.message || error.response.data.error;
-  } else {
-    return error.message;
-  }
+	if (
+		error &&
+		error.response &&
+		error.response.data &&
+		error.response.data.error
+	) {
+		return (
+			error.response.data.message ||
+			error.response.data.error
+		);
+	} else {
+		if (error.message === "Request failed with status code 404" || error.message === "Request failed with status code 401")
+			return 'Error! process failed';
+		return error.message;
+	}
 };
 
 export function toNormalDate(date) {
-  return new Date(date).toDateString();
+	return new Date(date).toDateString();
 }
 
-export function monthDiff(laterDate, earlierDate) {
-  const splittedLaterDate = laterDate.split("-");
-  const splittedEarlierDate = earlierDate.split("-");
-  let diff = differenceInCalendarMonths(
-    new Date(splittedLaterDate[0], splittedLaterDate[1], 1),
-    new Date(splittedEarlierDate[0], splittedEarlierDate[1], 1)
-  );
-  if (diff <= 0) {
-    return "(less than a month)";
-  } else if (diff > 0 && diff < 12) {
-    return `(${diff} months)`;
-  } else {
-    return `(${Math.round(diff / 12)} years)`;
-  }
+export function monthDiff(
+	laterDate,
+	earlierDate
+) {
+	const splittedLaterDate = laterDate.split('-');
+	const splittedEarlierDate = earlierDate.split(
+		'-'
+	);
+	let diff = differenceInCalendarMonths(
+		new Date(
+			splittedLaterDate[0],
+			splittedLaterDate[1],
+			1
+		),
+		new Date(
+			splittedEarlierDate[0],
+			splittedEarlierDate[1],
+			1
+		)
+	);
+	if (diff <= 0) {
+		return '(less than a month)';
+	} else if (diff > 0 && diff < 12) {
+		return `(${diff} months)`;
+	} else {
+		return `(${Math.round(diff / 12)} years)`;
+	}
 }
 
 export function yearDiff(laterDate, earlierDate) {
-  const splittedLaterDate = laterDate.split("-");
-  const splittedEarlierDate = earlierDate.split("-");
-  let diff = differenceInCalendarYears(
-    new Date(splittedLaterDate[0], splittedLaterDate[1], 1),
-    new Date(splittedEarlierDate[0], splittedEarlierDate[1], 1)
-  );
-  if (diff < 1) {
-    return splittedLaterDate[0];
-  } else if (diff > 1) {
-    return `${splittedEarlierDate[0]} - ${splittedLaterDate[0]}`;
-  } else {
-    return splittedLaterDate[0];
-  }
+	const splittedLaterDate = laterDate.split('-');
+	const splittedEarlierDate = earlierDate.split(
+		'-'
+	);
+	let diff = differenceInCalendarYears(
+		new Date(
+			splittedLaterDate[0],
+			splittedLaterDate[1],
+			1
+		),
+		new Date(
+			splittedEarlierDate[0],
+			splittedEarlierDate[1],
+			1
+		)
+	);
+	if (diff < 1) {
+		return splittedLaterDate[0];
+	} else if (diff > 1) {
+		return `${splittedEarlierDate[0]} - ${splittedLaterDate[0]}`;
+	} else {
+		return splittedLaterDate[0];
+	}
 }
 
-export function checkMyDateWithinRange(myDate, startDate = new Date()) {
-  if (startDate <= myDate) {
-    return "Please choose a future date";
-  }
-  return myDate;
+export function checkMyDateWithinRange(
+	myDate,
+	startDate = new Date()
+) {
+	if (startDate <= myDate) {
+		return 'Please choose a future date';
+	}
+	return myDate;
 }
