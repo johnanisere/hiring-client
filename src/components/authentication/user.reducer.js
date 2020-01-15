b6bd3250471214d97f2a82bcf16e7494a16655e1
whitehox <oketegah@gmail.com>
2020-01-15 17:23:24 +0100

    implemented get hirer method in user reducer

--- src/components/authentication/user.reducer.js
+++ src/components/authentication/user.reducer.js
@@ -5,9 +5,10 @@ import { UPDATE_DEV } from '../profile/actions/updateDetails.action';
 import { REGISTER_HIRER } from '../HirerSignUp/hirerSignup.action';
 import { HIRER_LOGIN } from '../HirerSignUp/hirerLogin/hirerLogin.action';
 import { SIGN_OUT } from './signout.action';
+import { GET_HIRER } from '../dashboard/hirer.action';
 export const initialState = {
   loading: false,
-  data: {}
+  data: {},
 };
 
 export default function user(state = initialState, action) {
@@ -17,32 +18,37 @@ export default function user(state = initialState, action) {
     case HIRER_LOGIN:
       return {
         ...state,
-        data: { ...state.data, ...action.payload }
+        data: { ...state.data, ...action.payload },
       };
     case USER_SIGNUP:
       return {
         ...state,
-        data: action.payload
+        data: action.payload,
+      };
+    case GET_HIRER:
+      return {
+        ...state,
+        data: action.payload.partner,
       };
     case 'LOADING':
       return {
         ...state,
-        loading: action.payload
+        loading: action.payload,
       };
 
     case 'CLEAR_SIGNUP_ERROR':
       return {
         ...state,
-        data: {}
+        data: {},
       };
     case REGISTER_HIRER:
       return {
         ...state,
-        data: action.payload
+        data: action.payload,
       };
     case SIGN_OUT:
       return {
-        ...initialState
+        ...initialState,
       };
     default:
       return state;
